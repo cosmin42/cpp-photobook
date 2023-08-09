43037bfb98a8ff82568f00ddb2228938b2e7a54a
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-09 15:01:17 +0300

    feat: Add names for all members.

--- PB/include/PBLog.h
+++ PB/include/PBLog.h
@@ -8,8 +8,35 @@ namespace PB {
 
 enum class LogLevel { None, Debug, Info, Warning, Error };
 
-template <> struct PBValue<LogLevel::None> {
-  static constexpr std::string_view name = "None";
-}
+template <> struct PBValue<LogLevel> {
+  static constexpr std::string_view name = "LogLevel";
+
+  template <LogLevel L> struct Members;
+
+  template<> struct Members<LogLevel::None>
+  {
+    static constexpr std::string_view name = "None";
+  };
+
+  template<> struct Members<LogLevel::Debug>
+  {
+    static constexpr std::string_view name = "Debug";
+  };
+
+  template<> struct Members<LogLevel::Info>
+  {
+    static constexpr std::string_view name = "Info";
+  };
+
+  template<> struct Members<LogLevel::Warning>
+  {
+    static constexpr std::string_view name = "Warning";
+  };
+
+  template<> struct Members<LogLevel::Error>
+  {
+    static constexpr std::string_view name = "Error";
+  };
+};
 
 } // namespace PB
