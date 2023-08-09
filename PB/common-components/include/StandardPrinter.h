883f6b41650fa0450ce908fc808567bb08892fc0
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-09 18:21:11 +0300

    feat: Add StandardPrinter content.

--- PB/common-components/include/StandardPrinter.h
+++ PB/common-components/include/StandardPrinter.h
@@ -9,10 +9,41 @@ public:
   SpecificPrinter() = default;
   ~SpecificPrinter() = default;
 
-  void printDebug(const char *format, ...) override {}
-  void printInfo(const char *format, ...) override {}
-  void printWarning(const char *format, ...) override {}
-  void printError(const char *format, ...) override {}
+  void printDebug(const char *format, ...) override {
+    va_list arglist;
+    va_start(arglist, format);
+    printf(
+        "[%s] ",
+        PB::PBValue<PB::LogLevel>::Members<PB::LogLevel::Debug>::name.data());
+    printf(format, arglist);
+    va_end(arglist);
+  }
+  void printInfo(const char *format, ...) override {
+    va_list arglist;
+    va_start(arglist, format);
+    printf("[%s] ",
+           PB::PBValue<PB::LogLevel>::Members<PB::LogLevel::Info>::name.data());
+    printf(format, arglist);
+    va_end(arglist);
+  }
+  void printWarning(const char *format, ...) override {
+    va_list arglist;
+    va_start(arglist, format);
+    printf(
+        "[%s] ",
+        PB::PBValue<PB::LogLevel>::Members<PB::LogLevel::Warning>::name.data());
+    printf(format, arglist);
+    va_end(arglist);
+  }
+  void printError(const char *format, ...) override {
+    va_list arglist;
+    va_start(arglist, format);
+    printf(
+        "[%s] ",
+        PB::PBValue<PB::LogLevel>::Members<PB::LogLevel::Error>::name.data());
+    printf(format, arglist);
+    va_end(arglist);
+  }
 };
 
 } // namespace CC
