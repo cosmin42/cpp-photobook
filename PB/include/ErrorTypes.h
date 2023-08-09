4320bbd0fd034f899d10466c0ec82ad71dc1b3a8
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-09 19:54:02 +0300

    feat: Add ErrorTypes.

---
+++ PB/include/ErrorTypes.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string_view>
+
+#include <util/Traits.h>
+
+namespace PB
+{
+enum class ErrorKind
+{
+    Unknown,
+    FileNotFound
+};
+
+template <> struct PBValue<ErrorKind> {
+  static constexpr std::string_view name = "ErrorKind";
+
+  template <ErrorKind E> struct Members;
+
+  template <> struct Members<ErrorKind::Unknown> {
+    static constexpr std::string_view name = "Unknown";
+  };
+
+  template <> struct Members<ErrorKind::FileNotFound> {
+    static constexpr std::string_view name = "FileNotFound";
+  };
+};
+}
