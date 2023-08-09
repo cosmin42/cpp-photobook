3b347f106a93dd7d73bae8354ff6ffb5b8232494
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-09 17:35:39 +0300

    style: Separate LogLevel.

--- PB/include/PBLog.h
+++ PB/include/PBLog.h
@@ -1,38 +1,8 @@
 #pragma once
 
-#include <string_view>
-
-#include <util/Traits.h>
+#include <LogLevel.h>
 
 namespace PB {
-
-enum class LogLevel { None, Debug, Info, Warning, Error };
-
-template <> struct PBValue<LogLevel> {
-  static constexpr std::string_view name = "LogLevel";
-
-  template <LogLevel L> struct Members;
-
-  template <> struct Members<LogLevel::None> {
-    static constexpr std::string_view name = "None";
-  };
-
-  template <> struct Members<LogLevel::Debug> {
-    static constexpr std::string_view name = "Debug";
-  };
-
-  template <> struct Members<LogLevel::Info> {
-    static constexpr std::string_view name = "Info";
-  };
-
-  template <> struct Members<LogLevel::Warning> {
-    static constexpr std::string_view name = "Warning";
-  };
-
-  template <> struct Members<LogLevel::Error> {
-    static constexpr std::string_view name = "Error";
-  };
-};
 
 class Printer {
 public:
