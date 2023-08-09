789857ed6f3dca32600ce53d8f0cb8111bf15fa0
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-09 15:57:41 +0300

    feat: Add print abstract methods.

--- PB/include/PBLog.h
+++ PB/include/PBLog.h
@@ -34,4 +34,15 @@ template <> struct PBValue<LogLevel> {
   };
 };
 
+class Printer {
+public:
+  virtual void printDebug(const char *format, ...) = 0;
+  virtual void printInfo(const char *format, ...) = 0;
+  virtual void printWarning(const char *format, ...) = 0;
+  virtual void printError(const char *format, ...) = 0;
+
+protected:
+  constexpr LogLevel mLevel = LogLevel::None;
+};
+
 } // namespace PB
