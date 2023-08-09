20673de8685199d72339bdb19b6fe5b326311e6d
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-09 13:18:08 +0300

    feat: Add LogLevel.

---
+++ PB/include/PBLog.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include <string_view>
+
+#include <util/Traits.h>
+
+namespace PB
+{
+
+enum class LogLevel
+{
+    None,
+    Debug,
+    Info,
+    Warning,
+    Error
+};
+
+template<>
+struct PBValue<LogLevel::None>
+{
+    static constexpr std::string_view str = "None";
+}
+
+}
