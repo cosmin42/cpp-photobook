d650ff147f09d5c3c4fec733b9d4333b2802cdfd
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-11 11:16:40 +0300

    tests: Use linking tests instead of specialized tests.

--- PB/tests/TestExiv2.cpp
+++
@@ -1,5 +0,0 @@
-#include <gtest/gtest.h>
-
-#include <exiv2/exiv2.hpp>
-
-TEST(TestExiv2, TestIncludeExiv2) {}
---
+++ PB/tests/TestThirdPartyLinks.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+#include <exiv2/exiv2.hpp>
+
+TEST(TestThirdPartyLinks, TestIncludeExiv2) {}
+
+TEST(TestThirdPartyLinks, TestIncludeUUID) {}
