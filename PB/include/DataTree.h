6f0db1a487236adabf557e784578ece530b04490
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-11 16:39:36 +0300

    feat: Add TreeNodeDataWrapper.

--- PB/include/DataTree.h
+++ PB/include/DataTree.h
@@ -1,3 +1,13 @@
 #pragma once
 
-namespace PB {}
+#include <NodeData.h>
+#include <unordered_map>
+
+#include <boost/uuid/uuid_generators.hpp>
+
+namespace PB {
+struct TreeNodeDataWrapper {
+  NodeData                                         data;
+  std::unordered_map<boost::uuids::uuid, NodeData> children;
+};
+} // namespace PB
