175dfa0716bda93b4acf7a4c998126e701e4437d
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-12 00:13:38 +0300

    feat: Add overloaded struct.

--- PB/include/util/Traits.h
+++ PB/include/util/Traits.h
@@ -4,4 +4,7 @@ namespace PB {
 
 template <typename T> struct PBValue;
 
+template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
 }
