a2c77ec458311fc970f4a617e9a6de38d6992d7c
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-22 19:02:22 +0300

    feat: Add extractPrefix functions.

--- PB/include/FileComparator.h
+++ PB/include/FileComparator.h
@@ -1,8 +1,15 @@
 #pragma once
 
+#include <compare>
+#include <string>
+
 namespace PB {
 struct CustomComparator {
-
   template <typename T> std::strong_ordering operator()(T const &, T const &);
+
+  static std::optional<std::string> extractPrefix(std::string const&);
+
+private:
+  static const std::string prefixRegex;
 };
 } // namespace PB
--- PB/src/FileComparator.cpp
+++ PB/src/FileComparator.cpp
@@ -2,8 +2,13 @@
 
 #include <cassert>
 #include <filesystem>
+#include <regex>
 
 namespace PB {
+
+const std::string CustomComparator::prefixRegex =
+    "^[[0-9]+(\.)][[0-9]+(\.)][0-9]+";
+
 template <>
 std::strong_ordering
 CustomComparator::operator()(std::filesystem::path const &a,
@@ -13,4 +18,18 @@ CustomComparator::operator()(std::filesystem::path const &a,
   assert(std::filesystem::exists(b) && "File doesn't exist.");
   return std::strong_ordering::greater;
 }
+
+std::optional<std::string> CustomComparator::extractPrefix(const std::string &input)
+{
+  std::regex  regexPattern(prefixRegex);
+  std::smatch match;
+
+  if (std::regex_search(input, match, regexPattern)) {
+    return match.str();
+  }
+  else {
+    return std::nullopt;
+  }
+}
+
 } // namespace PB
