ea148ba8759362160565b6e5632b0dd7ec59be23
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-22 16:37:58 +0300

    feat: Move print definition to cpp file.

---
+++ PB/src/ConfigDependants.cpp
@@ -0,0 +1,36 @@
+#include <ConfigDependants.h>
+
+#include <cstdarg>
+
+namespace PB {
+
+void printDebug(const char *format, ...)
+{
+  va_list arglist;
+  va_start(arglist, format);
+  sPrinter.printDebug(format, arglist);
+  va_end(arglist);
+}
+void printInfo(const char *format, ...)
+{
+  va_list arglist;
+  va_start(arglist, format);
+  sPrinter.printInfo(format, arglist);
+  va_end(arglist);
+}
+void printWarning(const char *format, ...)
+{
+  va_list arglist;
+  va_start(arglist, format);
+  sPrinter.printWarning(format, arglist);
+  va_end(arglist);
+}
+void printError(const char *format, ...)
+{
+  va_list arglist;
+  va_start(arglist, format);
+  sPrinter.printError(format, arglist);
+  va_end(arglist);
+}
+
+} // namespace PB
