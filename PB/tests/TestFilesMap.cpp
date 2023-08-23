a32a0097a1b77f09b58612b188259eff79547ae7
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-23 15:01:33 +0300

    test: Fix FilesMap test.

--- PB/tests/TestFilesMap.cpp
+++ PB/tests/TestFilesMap.cpp
@@ -11,10 +11,12 @@ TEST(TestFilesMap, TestConstructor)
   testing::internal::CaptureStdout();
   FilesMap    fileMapper(".");
   std::string output = testing::internal::GetCapturedStdout();
-
+#ifdef WIN32
+  ASSERT_EQ(output, "[Debug] FilesMap ctr .\n");
+#else
   ASSERT_EQ(output, "[Debug] FilesMap ctr \x10\n");
+#endif
 }
-
 
 TEST(TestFilesMap, TestSimpleCollection)
 {
