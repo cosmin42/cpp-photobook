bdd5025cbc7cdc603d85265742cd254b5ae5e68d
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-24 11:44:54 +0300

    feat: Use args handler in main.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
-#include <ArgsHandler.h>
+#include <PBArgsListener.h>
 
 auto main(int argc, char *argv[]) -> int
 {
+  BL::PBArgsListener argsListener;
+  BL::ArgsHandler handler(argsListener);
+  handler.handle(argc, argv);
   return 0;
 }
