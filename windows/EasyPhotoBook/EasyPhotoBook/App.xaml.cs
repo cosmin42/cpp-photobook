8a3a1508c8ddb5afa2db39b98fa37975bac4e42e
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-26 13:27:16 +0300

    feat: Add empty EasyPhotoBook.

---
+++ windows/EasyPhotoBook/EasyPhotoBook/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EasyPhotoBook
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
