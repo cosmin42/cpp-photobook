6c66892cbf097c367aa66ecac8e65e396697c725
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-29 14:16:14 +0300

    fix: Remove unused code.

--- PB/include/common/StandardPrinter.h
+++ PB/include/common/StandardPrinter.h
@@ -16,41 +16,24 @@ public:
 
   void doPrintDebug(const char *format, va_list arglist) const override
   {
-    //if constexpr (sLogLevel == LogLevel::None || sLogLevel > LogLevel::Debug) {
-    //  return;
-    //}
-
     constexpr auto tag = magic_enum::enum_name(LogLevel::Debug);
     printf("[%s] ", tag.data());
     vprintf(format, arglist);
   }
   void doPrintInfo(const char *format, va_list arglist) const override
   {
-    //if constexpr (sLogLevel == LogLevel::None || sLogLevel > LogLevel::Info) {
-    //  return;
-    //}
-
     constexpr auto tag = magic_enum::enum_name(LogLevel::Info);
     printf("[%s] ", tag.data());
     vprintf(format, arglist);
   }
   void doPrintWarning(const char *format, va_list arglist) const override
   {
-    //if constexpr (sLogLevel == LogLevel::None ||
-    //              sLogLevel > LogLevel::Warning) {
-    //  return;
-    //}
-
     constexpr auto tag = magic_enum::enum_name(LogLevel::Warning);
     printf("[%s] ", tag.data());
     vprintf(format, arglist);
   }
   void doPrintError(const char *format, va_list arglist) const override
   {
-    //if constexpr (sLogLevel == LogLevel::None) {
-    //  return;
-    //}
-
     constexpr auto tag = magic_enum::enum_name(LogLevel::Error);
     printf("[%s] ", tag.data());
     vprintf(format, arglist);
