00bf42f6200de4ac3166f45b59de0ad4df12e59a
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-29 23:45:41 +0300

    feat: Add Thread functionality.

--- PB/include/util/Thread.h
+++ PB/include/util/Thread.h
@@ -1,9 +1,32 @@
 #pragma once
+
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <queue>
 #include <thread>
+
+#include <Config.h>
 
 namespace PB {
 class Thread final {
 public:
+  explicit Thread(std::stop_token stopToken);
+  ~Thread() = default;
+
+  void post(std::function<void()> f);
+
 private:
+  void run();
+
+  void runNextTask();
+
+  auto getNextTask() -> std::function<void()>;
+
+  std::jthread                      mThread;
+  std::stop_token                   mToken;
+  std::queue<std::function<void()>> mTasksQueue;
+  std::mutex                        mTaskQueueAccess;
+  std::condition_variable_any       mQueueNotEmptyCondition;
 };
 } // namespace PB
--- PB/src/util/Thread.cpp
+++ PB/src/util/Thread.cpp
@@ -1,5 +1,38 @@
 #include <util/Thread.h>
-namespace PB
+namespace PB {
+Thread::Thread(std::stop_token stopToken) : mToken(stopToken)
 {
-
+  mThread = std::jthread([this]() { run(); });
 }
+
+void Thread::run()
+{
+  while (!mToken.stop_requested()) {
+    runNextTask();
+  }
+}
+
+void Thread::post(std::function<void()> f)
+{
+  std::scoped_lock exclusiveAccess(mTaskQueueAccess);
+  mTasksQueue.push(f);
+  mQueueNotEmptyCondition.notify_one();
+}
+
+void Thread::runNextTask() { getNextTask()(); }
+
+auto Thread::getNextTask() -> std::function<void()>
+{
+  std::unique_lock exclusiveAccess(mTaskQueueAccess);
+  bool interrupted = mQueueNotEmptyCondition.wait(exclusiveAccess, mToken, [&mTasksQueue = mTasksQueue]() {
+    return !mTasksQueue.empty();
+  });
+  if (interrupted)
+  {
+    return []() {};
+  }
+  auto f = mTasksQueue.front();
+  mTasksQueue.pop();
+  return f;
+}
+} // namespace PB
