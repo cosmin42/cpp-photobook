d72b3627d4d2a83b2a7a175623ca7fdeef6dc670
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-29 14:10:58 +0300

    feat: Add print functionality.

--- PB/include/common/Log.h
+++ PB/include/common/Log.h
@@ -11,18 +11,11 @@ class Log {
 public:
   void printDebug(const char *format, va_list arglist) const;
 
-  void printInfo(const char *format, va_list arglist) const
-  {
-    doPrintInfo(format, arglist);
-  }
-  void printWarning(const char *format, va_list arglist) const
-  {
-    doPrintWarning(format, arglist);
-  }
-  void printError(const char *format, va_list arglist) const
-  {
-    doPrintError(format, arglist);
-  }
+  void printInfo(const char *format, va_list arglist) const;
+
+  void printWarning(const char *format, va_list arglist) const;
+
+  void printError(const char *format, va_list arglist) const;
 
 private:
   virtual void doPrintDebug(const char *format, va_list arglist) const = 0;
--- PB/src/Log.cpp
+++ PB/src/Log.cpp
@@ -12,11 +12,41 @@ void Log::printDebug(const char *format, va_list arglist) const
   doPrintDebug(format, arglist);
 }
 
+void Log::printInfo(const char* format, va_list arglist) const
+{
+  if constexpr (Context::logLevel() == LogLevel::None ||
+                Context::logLevel() > LogLevel::Info) {
+    return;
+  }
+
+  doPrintInfo(format, arglist);
+}
+
+void Log::printWarning(const char* format, va_list arglist) const
+{
+  if constexpr (Context::logLevel() == LogLevel::None ||
+                Context::logLevel() > LogLevel::Warning) {
+    return;
+  }
+
+  doPrintWarning(format, arglist);
+}
+
+void Log::printError(const char* format, va_list arglist) const
+{
+  if constexpr (Context::logLevel() == LogLevel::None) {
+    return;
+  }
+
+  doPrintError(format, arglist);
+}
+
+
 void printDebug(const char *format, ...)
 {
   va_list arglist;
   va_start(arglist, format);
-  //sPrinter.printDebug(format, arglist);
+
   va_end(arglist);
 }
 void printInfo(const char *format, ...)
