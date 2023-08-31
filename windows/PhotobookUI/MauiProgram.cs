ba2988f88ea7b08ba0616ed91271af6c99be1626
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-31 17:58:15 +0300

    feat: Add toolkit package, hide menu bar.

--- windows/PhotobookUI/MauiProgram.cs
+++ windows/PhotobookUI/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace PhotobookUI
 {
@@ -7,16 +8,11 @@ namespace PhotobookUI
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+            builder.UseMauiApp<App>();
+            builder.UseMauiCommunityToolkit();
 
 #if DEBUG
-		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
