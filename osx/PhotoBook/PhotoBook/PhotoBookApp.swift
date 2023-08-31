280f752659ddaba27fffde8917d069d4b48d008b
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-31 15:40:42 +0300

    feat: Add empty xcode project.

---
+++ osx/PhotoBook/PhotoBook/PhotoBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoBookApp.swift
+//  PhotoBook
+//
+//  Created by Cosmin Mihai on 31.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
