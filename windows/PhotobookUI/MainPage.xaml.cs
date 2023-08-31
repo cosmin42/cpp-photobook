e3634d87804a163ea4a4966ad3335863bbf9eb7c
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-08-31 17:27:58 +0300

    feat: Replace the default UI.

--- windows/PhotobookUI/MainPage.xaml.cs
+++ windows/PhotobookUI/MainPage.xaml.cs
@@ -4,23 +4,14 @@ namespace PhotobookUI
 {
     public partial class MainPage : ContentPage, CppWinRTProjection.GradualControllableListener
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnNewProjectClicked(object sender, EventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
 
         public void OnFinished()
