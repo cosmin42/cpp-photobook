7fe1e7ac4fd03cef3b2a0e6197764a09f658747c
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-09-02 00:20:31 +0300

    feat: Add Folder picker.

--- windows/PhotobookUI/TableContentPage.xaml.cs
+++ windows/PhotobookUI/TableContentPage.xaml.cs
@@ -1,3 +1,4 @@
+using CommunityToolkit.Maui.Storage;
 using System.Diagnostics;
 
 namespace PhotobookUI;
@@ -16,6 +17,24 @@ public partial class TableContentPage : ContentPage
 
     private async void OnAddMediaClicked(object sender, EventArgs e)
     {
+
+        try
+        {
+            var result = await FolderPicker.PickAsync(CancellationToken.None);
+
+            if (result != null)
+            {
+            }
+            else
+            {
+                Debug.Write("Folder was not chosen.");
+            }
+        }
+        catch
+        {
+            Debug.WriteLine("Failed to choose folder.");
+        }
+
         Debug.WriteLine("Add Media clicked.");
     }
 
