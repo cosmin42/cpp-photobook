b01eed628453d27627368e58f86843b669b43a9b
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-09-01 23:27:24 +0300

    ui: Add Media clicked button.

--- windows/PhotobookUI/TableContentPage.xaml.cs
+++ windows/PhotobookUI/TableContentPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace PhotobookUI;
 
 public partial class TableContentPage : ContentPage
@@ -10,6 +12,11 @@ public partial class TableContentPage : ContentPage
     private async void OnBackClicked(object sender, EventArgs e)
     {
         await Navigation.PopAsync();
+    }
+
+    private async void OnAddMediaClicked(object sender, EventArgs e)
+    {
+        Debug.WriteLine("Add Media clicked.");
     }
 
 }
