2d1141aeb8d0c7e4572088d2d99eb7ec33bdd11e
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-09-03 21:13:56 +0300

    feat: Remove GradualControllable.

--- PB/include/pb/GradualControllable.h
+++ PB/include/pb/GradualControllable.h
@@ -15,37 +15,4 @@ public:
   virtual void onProgressUpdate() = 0;
   virtual void onError(Error error) = 0;
 };
-
-class GradualControllable {
-public:
-  explicit GradualControllable(GradualControllableListener &listener)
-      : mListener(listener)
-  {
-  }
-  virtual ~GradualControllable() = default;
-
-  void start();
-  void stop();
-  void pause();
-  void resume();
-  void finish();
-
-  bool isStopped() const;
-  bool isPaused() const;
-
-protected:
-  GradualControllableListener &mListener;
-
-private:
-  virtual void doStart() = 0;
-  virtual void doStop() = 0;
-  virtual void doPause() = 0;
-  virtual void doResume() = 0;
-  virtual void doFinish() = 0;
-
-  void wait();
-
-  bool mStopped = true;
-  bool mPaused = false;
-};
 } // namespace PB
--- PB/src/GradualControllable.cpp
+++ PB/src/GradualControllable.cpp
@@ -1,44 +1,6 @@
 #include <pb/GradualControllable.h>
 
-#include <thread>
 
 namespace PB {
-void GradualControllable::start()
-{
-  if (!mStopped) {
-    return;
-  }
 
-  mStopped = false;
-  doStart();
-}
-void GradualControllable::stop()
-{
-  doStop();
-  mListener.onStopped();
-  mStopped = true;
-}
-
-void GradualControllable::finish()
-{
-  doFinish();
-  mListener.onFinished();
-  mStopped = true;
-}
-
-void GradualControllable::pause()
-{
-  doPause();
-  mPaused = true;
-}
-void GradualControllable::resume()
-{
-  mPaused = false;
-  doResume();
-}
-
-bool GradualControllable::isStopped() const { return mStopped; }
-bool GradualControllable::isPaused() const { return mPaused; }
-
-void GradualControllable::wait() {  }
 } // namespace PB
