624da95684b14dc15b9374789d35bd5c6a967210
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-09-08 00:48:20 +0300

    feat: Implement post function.

--- windows/CppWinRTProjection/ScheduableWinRT.cpp
+++ windows/CppWinRTProjection/ScheduableWinRT.cpp
@@ -2,6 +2,11 @@
 
 namespace winrt::CppWinRTProjection::implementation {
 
-void WinRTMainThread::post(std::function<void()>){};
+void WinRTMainThread::post(std::function<void()> f)
+{
+  mMainThreadDispatcher.RunAsync(
+      winrt::Windows::UI::Core::CoreDispatcherPriority::Normal,
+                                 std::forward<std::function<void()>>(f));
+}
 
 } // namespace winrt::CppWinRTProjection::implementation
