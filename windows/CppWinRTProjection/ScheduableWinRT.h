47fe905ca6893abd02a1a7773402695e6bc80701
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-09-07 19:08:07 +0300

    feat: Add CoreDispatcher.

--- windows/CppWinRTProjection/ScheduableWinRT.h
+++ windows/CppWinRTProjection/ScheduableWinRT.h
@@ -2,10 +2,15 @@
 
 #include <pb/Scheduable.h>
 
+#include <winrt/Windows.UI.Core.h>
+
 namespace winrt::CppWinRTProjection::implementation {
 class WinRTMainThread final : PB::Scheduable {
 public:
   void post(std::function<void()>) override;
+
+private:
+  winrt::Windows::UI::Core::CoreDispatcher mMainThreadDispatcher;
 };
 
 } // namespace winrt::CppWinRTProjection::implementation
