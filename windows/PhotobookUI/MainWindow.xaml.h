e7f666a77b0c641800fa7db8cdeca739c013b1b1
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-09-09 02:03:52 +0300

    feat: Add PhotobookUI WinUI project.

---
+++ windows/PhotobookUI/MainWindow.xaml.h
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::PhotobookUI::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::PhotobookUI::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
