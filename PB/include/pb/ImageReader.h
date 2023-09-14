1e176a63926fa1c650e601460c746fcec015485d
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-09-14 22:01:39 +0300

    feat: Add a reference to CircularIterator in the image reader.

--- PB/include/pb/ImageReader.h
+++ PB/include/pb/ImageReader.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <filesystem>
+#include <map>
 #include <optional>
 #include <string>
 
@@ -8,16 +9,25 @@
 #pragma warning(disable : 5054)
 #pragma warning(disable : 4127)
 #include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
 #pragma warning(pop)
 
+#include <pb/Config.h>
+#include <pb/FileMapper.h>
+
 namespace PB {
+
+template <typename T>
+  requires TaskManageableConcept<T>
 class ImageReader final {
 public:
-  ImageReader() = delete;
+  // How many images are loaded beside the main one
+  static constexpr unsigned sBufferSize = 3 * 2;
+  ImageReader() = default;
   ImageReader(ImageReader const &) = delete;
   ImageReader(ImageReader &&) = delete;
   ImageReader &operator=(ImageReader const &other) = delete;
-  ~ImageReader() = delete;
+  ~ImageReader() = default;
 
   static auto defaultRead() -> auto
   {
@@ -27,6 +37,21 @@ public:
   }
 
 private:
-  static auto read(std::filesystem::path const &path) -> std::optional<cv::Mat>;
+  static auto read(std::filesystem::path const &path) -> std::optional<cv::Mat>
+  {
+    cv::Mat image = cv::imread(path.string(), cv::IMREAD_COLOR);
+
+    if (image.empty()) {
+      printDebug("Image %s could not be read", path.string().c_str());
+      return std::nullopt;
+    }
+
+    return image;
+  }
+
+  std::map<Path, std::shared_ptr<cv::Mat>> mBuffer;
+  std::vector<Path>                        mBufferIndex;
+
+  std::optional<CircularIterator<MediaMapper<T>, Path>> mIterator;
 };
 } // namespace PB
--- PB/src/ImageReader.cpp
+++ PB/src/ImageReader.cpp
@@ -6,19 +6,6 @@
 #include <opencv2/imgcodecs.hpp>
 #pragma warning(pop)
 
-#include <pb/Config.h>
-
 namespace PB {
-auto ImageReader::read(std::filesystem::path const &path)
-    -> std::optional<cv::Mat>
-{
-  cv::Mat image = cv::imread(path.string(), cv::IMREAD_COLOR);
 
-  if (image.empty()) {
-    printDebug("Image %s could not be read", path.string().c_str());
-    return std::nullopt;
-  }
-
-  return image;
-}
 } // namespace PB
