5b7dc997b3f0c2933870bea2b7c11a5214c258f2
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-09-15 17:45:09 +0300

    feat: Refactor FileMapper to remove uneeded generic parameter.

--- PB/include/pb/FileMapper.h
+++ PB/include/pb/FileMapper.h
@@ -7,82 +7,19 @@
 #include <pb/CircularIterator.h>
 #include <pb/Config.h>
 #include <pb/FileComparator.h>
+#include <pb/MediaMapListener.h>
 #include <pb/util/Concepts.h>
 #include <pb/util/Thread.h>
 #include <pb/util/Traits.h>
 
 namespace PB {
 
-class MediaMap final {
+template <typename TaskManageableType> class MediaMapper final : public Thread {
 public:
-  MediaMap() = default;
-
-  explicit MediaMap(std::vector<std::filesystem::path> &paths)
-  {
-    mPaths.reserve(paths.size());
-    std::copy(paths.begin(), paths.end(), paths.begin());
-  }
-
-  ~MediaMap() = default;
-
-  void add(std::filesystem::path &path)
-  {
-    bool isDirectory = std::filesystem::is_directory(path);
-    bool isRegularFile = std::filesystem::is_regular_file(path);
-
-    if (isRegularFile && validEndtry(path)) {
-      mPaths.push_back(path);
-    }
-
-    if (isDirectory) {
-      mPaths.push_back(path);
-    }
-  }
-
-  [[nodiscard]] auto size() const -> unsigned
-  {
-    return (unsigned)mPaths.size();
-  }
-
-  auto access(unsigned index) const -> std::optional<Path>
-  {
-    if (index >= mPaths.size()) {
-      return std::nullopt;
-    }
-    return mPaths.at(index);
-  }
-
-  [[nodiscard]] auto iterator() -> std::optional<CircularIterator>
-  {
-    return CircularIterator(mPaths);
-  }
-
-private:
-  bool validEndtry(Path path) const
-  {
-    static const std::set<std::string> sValidFileExtensions = {"jpg", "jpeg",
-                                                               "png"};
-    // TODO: C++23 to be replaced with ends with
-    std::string pathStr = path.string();
-    for (auto &extension : sValidFileExtensions) {
-      bool endsWith =
-          std::equal(extension.rbegin(), extension.rend(), pathStr.rbegin());
-      if (endsWith) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  std::vector<std::filesystem::path> mPaths;
-};
-
-template <typename T>
-  requires TaskManageableConcept<T>
-class MediaMapper final : public Thread {
-public:
-  explicit MediaMapper(std::filesystem::path const &root, T &listener)
-      : Thread(Context::inst().sStopSource.get_token()), mListener(listener)
+  explicit MediaMapper(std::filesystem::path const          &root,
+                       MediaMapListener<TaskManageableType> &listener)
+      : Thread(Context::inst().sStopSource.get_token()), mListener(listener),
+        mRoot(root)
   {
     printDebug("MediaMapper constructor.\n");
     mRecursiveIterator = std::filesystem::recursive_directory_iterator(
@@ -91,13 +28,13 @@ public:
 
   MediaMapper(MediaMapper const &other)
       : Thread(other), mRecursiveIterator(other.mRecursiveIterator),
-        mListener(other.mListener)
+        mListener(other.mListener), mRoot(other.mRoot)
   {
   }
 
   MediaMapper(MediaMapper &&other) noexcept
       : Thread(other), mRecursiveIterator(other.mRecursiveIterator),
-        mListener(other.mListener)
+        mListener(other.mListener), mRoot(other.mRoot)
   {
   }
 
@@ -117,14 +54,15 @@ public:
     mListener.onProgressUpdate();
   }
 
-  void finish() override { mListener.onFinished(); }
+  void finish() override { mListener.onFinished(mMap, mRoot); }
 
   auto map() const -> MediaMap { return mMap; }
 
 private:
   std::filesystem::recursive_directory_iterator mRecursiveIterator;
   MediaMap                                      mMap;
-  T                                            &mListener;
+  MediaMapListener<TaskManageableType>         &mListener;
+  Path                                          mRoot;
 };
 
 } // namespace PB
