c897abfeaccb66f898200d3c0280c876276d56c1
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-09-19 00:46:08 +0300

    dev: Remove unused includes.

--- PB/src/ImageSetWriter.cpp
+++ PB/src/ImageSetWriter.cpp
@@ -1,12 +1,5 @@
 #include <pb/ImageSetWriter.h>
 
-#pragma warning(push)
-#pragma warning(disable : 5054)
-#pragma warning(disable : 4127)
-#include <opencv2/imgcodecs.hpp>
-#pragma warning(pop)
-
-#include <pb/Config.h>
 
 namespace PB {
 
