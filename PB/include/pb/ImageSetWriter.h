dda43696df27f3b491d63c1bf178322b354dc98c
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-09-18 23:29:41 +0300

    dev: Develop ImageSetWriter.
    Remove the old method, add the container parametrized variants.
    Add support functions in the class.

--- PB/include/pb/ImageSetWriter.h
+++ PB/include/pb/ImageSetWriter.h
@@ -4,10 +4,15 @@
 #pragma warning(disable : 5054)
 #pragma warning(disable : 4127)
 #include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
 #pragma warning(pop)
 
 #include <filesystem>
+#include <tuple>
 #include <vector>
+
+#include <pb/common/Log.h>
+#include <pb/util/Traits.h>
 
 namespace PB {
 class ImageSetWriter final {
@@ -18,7 +23,48 @@ public:
   ImageSetWriter &operator=(ImageSetWriter const &other) = delete;
   ~ImageSetWriter() = default;
 
-  void writeImages(std::vector<cv::Mat> const  &images,
-                   std::filesystem::path const &path) const;
+  template <template <typename> typename T>
+  void writeImages(T<std::shared_ptr<cv::Mat>> const &images, Path folder) const
+  {
+    int counter = 0;
+
+    for (auto imagePtr : images) {
+      auto path = makePath(folder, counter);
+      write(path, imagePtr);
+      counter++;
+    }
+  }
+
+  template <template <typename, typename> typename T>
+  void writeImages(T<Path, std::shared_ptr<cv::Mat>> const &images,
+                   Path const                              &folder) const
+  {
+    int counter = 0;
+    for (auto [key, value] : images) {
+      Unused(key);
+      auto path = makePath(folder, counter);
+      write(path, value);
+      counter++;
+    }
+  }
+
+private:
+  void write(Path const &path, std::shared_ptr<cv::Mat> image)
+  {
+    bool success = cv::imwrite(path.string(), *image);
+    if (!success) {
+      printDebug("File could not be saved to %s", path.string().c_str());
+    }
+  }
+
+  Path makePath(Path path, int counter)
+  {
+    const std::string prefix = "image_";
+    ;
+    std::string fileNameStr = prefix + std::to_string(counter) + ".jpg";
+    std::filesystem::path file(fileNameStr);
+    path = path / file;
+    return path;
+  }
 };
 } // namespace PB
--- PB/src/ImageSetWriter.cpp
+++ PB/src/ImageSetWriter.cpp
@@ -9,12 +9,5 @@
 #include <pb/Config.h>
 
 namespace PB {
-void ImageSetWriter::writeImages(std::vector<cv::Mat> const  &image,
-                                 std::filesystem::path const &path) const
-{
-  bool success = cv::imwrite(path.string(), image);
-  if (!success) {
-    printDebug("File could not be saved to %s", path.string().c_str());
-  }
-}
+
 } // namespace PB
