267cbf8ca1b9f3dc083af9694585ce94e7982305
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-09-19 10:09:30 +0300

    dev: Add copy/move/assign for `Error`.

--- PB/include/pb/Error.h
+++ PB/include/pb/Error.h
@@ -9,27 +9,45 @@ namespace PB {
 class [[nodiscard]] Error final {
 public:
   Error() = default;
+  Error(Error const &other)
+      : mDescription(other.mDescription), mErrorKind(mErrorKind)
+  {
+  }
+  Error(Error &&other) noexcept
+      : mDescription(other.mDescription), mErrorKind(mErrorKind)
+  {
+  }
+
+  Error &operator=(Error const &other)
+  {
+    mDescription = other.mDescription;
+    mErrorKind = other.mErrorKind;
+    return *this;
+  }
   ~Error() = default;
 
   [[nodiscard]] auto kind() const noexcept -> ErrorKind { return mErrorKind; }
 
-  [[nodiscard]] auto description() const noexcept -> std::string {
+  [[nodiscard]] auto description() const noexcept -> std::string
+  {
     return mDescription;
   }
 
-  auto operator<<(ErrorKind kind) noexcept -> Error {
+  auto operator<<(ErrorKind kind) noexcept -> Error
+  {
     mErrorKind = kind;
     return *this;
   }
-  auto operator<<(std::string description) noexcept -> Error {
+  auto operator<<(std::string description) noexcept -> Error
+  {
     mDescription = description;
     return *this;
   }
 
 private:
-  std::string mDescription;
-  ErrorKind mErrorKind = ErrorKind::Unknown;
-  uint8_t padding[4];
+  std::string mDescription = "";
+  ErrorKind   mErrorKind = ErrorKind::Unknown;
+  uint8_t     padding[4];
 };
 
 } // namespace PB
