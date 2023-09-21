97519bb0e9828a85d8d8d506466b86a0cefd8097
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-09-21 10:39:57 +0300

    fix: Use  for macos.

--- build-pblib.sh
+++ build-pblib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cmake -B PB/build -S PB -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -DLOG_LEVEL=Debug -DPRINTER_TYPE=OutputConsolePrinter
+CXX=clang++ CC=clang cmake -B PB/build -S PB -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -DLOG_LEVEL=Debug -DPRINTER_TYPE=StandardPrinter
