21278e792a0c3f13176cc99749b118df8ca814db
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-09-25 10:51:34 +0300

    dev: Add `Project.h`.

---
+++ PB/include/pb/Project.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+
+#include <pb/Config.h>
+#include <pb/util/Traits.h>
+
+namespace PB {
+struct ProjectDetails {
+  boost::uuids::uuid uuid;
+  std::string        name;
+  Path               parentDirectory;
+};
+
+class Project final {
+public:
+  Project()
+  {
+    mProjectDetails.uuid = boost::uuids::random_generator()();
+    mProjectDetails.name = "Untitled";
+  }
+  ~Project() = default;
+
+  ProjectDetails &details() { return mProjectDetails; }
+
+private:
+  ProjectDetails mProjectDetails;
+};
+} // namespace PB
