90b944444ddd5ca3643e1f59e82fb6b55f4976ad
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-09-28 20:43:26 +0300

    dev: Add `Project.cpp`

--- PB/include/pb/Project.h
+++ PB/include/pb/Project.h
@@ -27,53 +27,7 @@ struct ProjectDetails {
 };
 
 std::variant<ProjectDetails, Error>
-convert(std::unordered_map<std::string, std::string> const &map)
-{
-  ProjectDetails projectDetails;
-
-  auto projectUUID = PB::mapGet<std::unordered_map, std::string, std::string>(
-      map, std::string("project-uuid"));
-
-  if (!projectUUID) {
-    return Error() << ErrorCode::InvalidProjectDescription;
-  }
-
-  auto projectName = PB::mapGet<std::unordered_map, std::string, std::string>(
-      map, std::string("project-name"));
-
-  if (!projectName) {
-    return Error() << ErrorCode::InvalidProjectDescription;
-  }
-
-  auto projectPath = PB::mapGet<std::unordered_map, std::string, std::string>(
-      map, std::string("project-path"));
-
-  if (!projectPath) {
-    return Error() << ErrorCode::InvalidProjectDescription;
-  }
-
-  boost::uuids::uuid newUUID;
-
-  try {
-    boost::uuids::string_generator gen;
-    newUUID = gen(*projectUUID);
-  }
-  catch (...) {
-    return Error() << ErrorCode::InvalidUUID;
-  }
-
-  Path newPath = *projectPath;
-
-  if (!std::filesystem::exists(newPath)) {
-    return Error() << ErrorCode::ProjectPathDoesNotExist;
-  }
-
-  projectDetails.uuid = newUUID;
-  projectDetails.name = *projectName;
-  projectDetails.parentDirectory = newPath;
-
-  return projectDetails;
-}
+convert(std::unordered_map<std::string, std::string> const &map);
 
 template <typename PersistenceType> class Project final {
 public:
---
+++ PB/src/Project.cpp
@@ -0,0 +1,53 @@
+#include <pb/Project.h>
+
+namespace PB {
+
+std::variant<ProjectDetails, Error>
+convert(std::unordered_map<std::string, std::string> const &map)
+{
+  ProjectDetails projectDetails;
+
+  auto projectUUID = PB::mapGet<std::unordered_map, std::string, std::string>(
+      map, std::string("project-uuid"));
+
+  if (!projectUUID) {
+    return Error() << ErrorCode::InvalidProjectDescription;
+  }
+
+  auto projectName = PB::mapGet<std::unordered_map, std::string, std::string>(
+      map, std::string("project-name"));
+
+  if (!projectName) {
+    return Error() << ErrorCode::InvalidProjectDescription;
+  }
+
+  auto projectPath = PB::mapGet<std::unordered_map, std::string, std::string>(
+      map, std::string("project-path"));
+
+  if (!projectPath) {
+    return Error() << ErrorCode::InvalidProjectDescription;
+  }
+
+  boost::uuids::uuid newUUID;
+
+  try {
+    boost::uuids::string_generator gen;
+    newUUID = gen(*projectUUID);
+  }
+  catch (...) {
+    return Error() << ErrorCode::InvalidUUID;
+  }
+
+  Path newPath = *projectPath;
+
+  if (!std::filesystem::exists(newPath)) {
+    return Error() << ErrorCode::ProjectPathDoesNotExist;
+  }
+
+  projectDetails.uuid = newUUID;
+  projectDetails.name = *projectName;
+  projectDetails.parentDirectory = newPath;
+
+  return projectDetails;
+}
+} // namespace PB
