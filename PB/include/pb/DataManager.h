b667fc81da3074363b3a4a20e97f67aef0a746ab
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-09-29 18:22:11 +0300

    dev: Add member for storing the link between images path - thumbnails.

--- PB/include/pb/DataManager.h
+++ PB/include/pb/DataManager.h
@@ -6,19 +6,22 @@
 
 namespace PB {
 
-class DataManager final{
+class DataManager final {
 public:
   std::unordered_map<Path, MediaMap> &mediaData() { return mMediaData; }
   std::vector<Path> &mediaIndexedByType() { return mMediaIndexedByPath; }
+  std::unordered_map<Path, Path> &smallThumbnails() { return mThumbnailsSmall; }
 
   void clear()
   {
     mMediaIndexedByPath.clear();
     mMediaData.clear();
+    mThumbnailsSmall.clear();
   }
 
 private:
   std::unordered_map<Path, MediaMap> mMediaData;
   std::vector<Path>                  mMediaIndexedByPath;
+  std::unordered_map<Path, Path>     mThumbnailsSmall;
 };
 } // namespace PB
