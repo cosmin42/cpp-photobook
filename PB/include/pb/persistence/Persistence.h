585804b7bb87908116435c64e5378757ed2d0bf5
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-10-10 23:52:23 +0300

    dev: Add implementation for sqlite persistence read/write.

--- PB/include/pb/persistence/Persistence.h
+++ PB/include/pb/persistence/Persistence.h
@@ -36,10 +36,139 @@ public:
     return Error() << ErrorCode::SQLiteError << std::to_string(success);
   }
 
+  void
+  read(std::function<
+       void(std::variant<std::unordered_map<std::string, std::string>, Error>)>
+           onReturn)
+  {
+    auto maybeError = createProjectsRegisterIfNotExisting();
+    if (maybeError.has_value()) {
+      onReturn(maybeError.value());
+      return;
+    }
+
+    std::unordered_map<std::string, std::string> readMap;
+
+    sqlite3_stmt *stmt;
+    auto success = sqlite3_prepare_v2(mDatabaseHandle.get(), SELECT_PROJECTS,
+                                      -1, &stmt, nullptr);
+
+    if (success != SQLITE_OK) {
+      onReturn(Error() << ErrorCode::SQLiteError << std::to_string(success));
+      return;
+    }
+
+    while (sqlite3_step(stmt) == SQLITE_ROW) {
+      const char *uuid =
+          reinterpret_cast<const char *>(sqlite3_column_text(stmt, 1));
+      const char *path =
+          reinterpret_cast<const char *>(sqlite3_column_text(stmt, 2));
+      readMap[uuid] = path;
+    }
+
+    sqlite3_finalize(stmt);
+  }
+
+  void write(std::unordered_map<std::string, std::string> map,
+             std::function<void(std::optional<Error>)>    onReturn)
+  {
+    auto maybeError = createProjectsRegisterIfNotExisting();
+    if (maybeError.has_value()) {
+      onReturn(maybeError.value());
+      return;
+    }
+
+    for (auto &[key, value] : map) {
+      auto maybePairOrError = queryProjectEntry(key);
+
+      if (std::holds_alternative<Error>(maybePairOrError)) {
+        onReturn(std::get<Error>(maybePairOrError));
+      }
+      else {
+        auto &maybePair =
+            std::get<std::optional<std::pair<std::string, std::string>>>(
+                maybePairOrError);
+
+        std::string query;
+        if (maybePair.has_value()) {
+          query = "UPDATE PROJECTS_REGISTER SET path = '" + value +
+                  "' WHERE uuid = '" + key + "';";
+        }
+        else {
+          query = "INSERT INTO PROJECTS_REGISTER (uuid, path) VALUES ('" + key +
+                  "', '" + value + "');";
+        }
+
+        char *errMsg = nullptr;
+
+        auto success = sqlite3_exec(mDatabaseHandle.get(), query.c_str(),
+                                    nullptr, nullptr, &errMsg);
+
+        if (success != SQLITE_OK) {
+          sqlite3_free(errMsg);
+          onReturn(Error() << ErrorCode::SQLiteError);
+          return;
+        }
+      }
+    }
+
+    onReturn(std::nullopt);
+  }
+
 private:
+  std::optional<Error> createProjectsRegisterIfNotExisting()
+  {
+    char *errMsg = nullptr;
+    auto success = sqlite3_exec(mDatabaseHandle.get(), CREATE_PROJECTS_REGISTER,
+                                nullptr, nullptr, &errMsg);
+
+    if (success != SQLITE_OK) {
+      sqlite3_free(errMsg);
+      return Error() << ErrorCode::SQLiteError << std::to_string(success);
+    }
+
+    return std::nullopt;
+  }
+
+  std::variant<std::optional<std::pair<std::string, std::string>>, Error>
+  queryProjectEntry(std::string searchedUUID)
+  {
+    std::string query = "SELECT " + searchedUUID + " FROM PROJECTS_REGISTER";
+
+    sqlite3_stmt *stmt;
+    auto success = sqlite3_prepare_v2(mDatabaseHandle.get(), SELECT_PROJECTS,
+                                      -1, &stmt, nullptr);
+
+    if (success != SQLITE_OK) {
+      return Error() << ErrorCode::SQLiteError << std::to_string(success);
+    }
+
+    while (sqlite3_step(stmt) == SQLITE_ROW) {
+      const char *uuid =
+          reinterpret_cast<const char *>(sqlite3_column_text(stmt, 1));
+      const char *path =
+          reinterpret_cast<const char *>(sqlite3_column_text(stmt, 2));
+      sqlite3_finalize(stmt);
+      return std::pair<std::string, std::string>(uuid, path);
+    }
+
+    sqlite3_finalize(stmt);
+    return std::nullopt;
+  }
+
   static constexpr const char *DATABASE_NAME = "database.db";
-  Path                         mPath;
-  std::shared_ptr<sqlite3>     mDatabaseHandle = nullptr;
+  static constexpr const char *CREATE_PROJECTS_REGISTER =
+      "CREATE TABLE IF NOT EXISTS PROJECTS_REGISTER ("
+      "    id INTEGER PRIMARY KEY AUTOINCREMENT,"
+      "    uuid TEXT NOT NULL,"
+      "    path TEXT"
+      ");";
+
+  static constexpr const char *SELECT_PROJECTS =
+      "SELECT * FROM PROJECTS_REGISTER;";
+
+  Path                     mPath;
+  std::shared_ptr<sqlite3> mDatabaseHandle = nullptr;
 };
 
 template <typename PersistenceType = void> class Persistence final {
