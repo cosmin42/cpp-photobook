f94d79baf884c382f61140b343ccf460079e5f65
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-10-11 12:28:06 +0300

    tests: Add test for deleting existent/non-existent key.

--- PB/tests/TestPersistence.cpp
+++ PB/tests/TestPersistence.cpp
@@ -216,3 +216,116 @@ TEST(TestPersistence, InsertEntry)
         });
   });
 }
+
+TEST(TestPersistence, DeleteEntry)
+{
+  std::filesystem::remove(SQLitePersistence::DATABASE_NAME);
+  std::filesystem::remove(Context::PERSISTENCE_FILENAME);
+
+  PB::SQLitePersistence persistence(".");
+
+  auto maybeError = persistence.connect();
+  ASSERT_TRUE(!maybeError);
+
+  std::unordered_map<std::string, std::string> map{{"testKey", "testValue"}};
+
+  persistence.write(map, [map](std::optional<PB::Error> maybeError) {
+    ASSERT_TRUE(!maybeError.has_value());
+    PB::SQLitePersistence persistence(".");
+    persistence.connect();
+    persistence.read(
+        [map](std::variant<std::unordered_map<std::string, std::string>, Error>
+                  mapOrError) {
+          ASSERT_TRUE(!std::holds_alternative<Error>(mapOrError));
+          auto &secondMap =
+              std::get<std::unordered_map<std::string, std::string>>(
+                  mapOrError);
+          ASSERT_TRUE(map.size() == secondMap.size());
+
+          for (auto &[key, value] : map) {
+            ASSERT_TRUE(secondMap.find(key) != secondMap.end());
+            ASSERT_TRUE(secondMap.at(key) == map.at(key));
+          }
+        });
+  });
+
+  map["testKey"] = "testValue2";
+  map.clear();
+
+  persistence.deleteEntry("testKey", [map](
+                                         std::optional<PB::Error> maybeError) {
+    ASSERT_TRUE(!maybeError.has_value());
+    PB::SQLitePersistence persistence(".");
+    persistence.connect();
+    persistence.read(
+        [map](std::variant<std::unordered_map<std::string, std::string>, Error>
+                  mapOrError) {
+          ASSERT_TRUE(!std::holds_alternative<Error>(mapOrError));
+          auto &secondMap =
+              std::get<std::unordered_map<std::string, std::string>>(
+                  mapOrError);
+          ASSERT_TRUE(map.size() == secondMap.size());
+
+          for (auto &[key, value] : map) {
+            ASSERT_TRUE(secondMap.find(key) != secondMap.end());
+            ASSERT_TRUE(secondMap.at(key) == map.at(key));
+          }
+        });
+  });
+}
+
+TEST(TestPersistence, DeleteNonExistentEntry)
+{
+  std::filesystem::remove(SQLitePersistence::DATABASE_NAME);
+  std::filesystem::remove(Context::PERSISTENCE_FILENAME);
+
+  PB::SQLitePersistence persistence(".");
+
+  auto maybeError = persistence.connect();
+  ASSERT_TRUE(!maybeError);
+
+  std::unordered_map<std::string, std::string> map{{"testKey", "testValue"}};
+
+  persistence.write(map, [map](std::optional<PB::Error> maybeError) {
+    ASSERT_TRUE(!maybeError.has_value());
+    PB::SQLitePersistence persistence(".");
+    persistence.connect();
+    persistence.read(
+        [map](std::variant<std::unordered_map<std::string, std::string>, Error>
+                  mapOrError) {
+          ASSERT_TRUE(!std::holds_alternative<Error>(mapOrError));
+          auto &secondMap =
+              std::get<std::unordered_map<std::string, std::string>>(
+                  mapOrError);
+          ASSERT_TRUE(map.size() == secondMap.size());
+
+          for (auto &[key, value] : map) {
+            ASSERT_TRUE(secondMap.find(key) != secondMap.end());
+            ASSERT_TRUE(secondMap.at(key) == map.at(key));
+          }
+        });
+  });
+
+  map["testKey"] = "testValue2";
+
+  persistence.deleteEntry("non ExistentKey", [map](
+                                         std::optional<PB::Error> maybeError) {
+    ASSERT_TRUE(!maybeError.has_value());
+    PB::SQLitePersistence persistence(".");
+    persistence.connect();
+    persistence.read(
+        [map](std::variant<std::unordered_map<std::string, std::string>, Error>
+                  mapOrError) {
+          ASSERT_TRUE(!std::holds_alternative<Error>(mapOrError));
+          auto &secondMap =
+              std::get<std::unordered_map<std::string, std::string>>(
+                  mapOrError);
+          ASSERT_TRUE(map.size() == secondMap.size());
+
+          for (auto &[key, value] : map) {
+            ASSERT_TRUE(secondMap.find(key) != secondMap.end());
+            ASSERT_TRUE(secondMap.at(key) == map.at(key));
+          }
+        });
+  });
+}
