a07746ac9a838a1ae7b889aaab83658ad3f672dc
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-10-22 19:48:59 +0300

    dev: Android create project.

---
+++ android/app/src/main/java/com/mandelbrotcode/photobook/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.mandelbrotcode.photobook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.mandelbrotcode.photobook.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'photobook' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'photobook' library on application startup.
+        init {
+            System.loadLibrary("photobook")
+        }
+    }
+}
