ed2c2b7b478be877c41b702c0f9c40871f8d8501
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2023-10-29 22:42:33 +0200

    ui: Rename ContentView to DasboardView and add TableContentView.

--- osx/PhotoBook/PhotoBook/DashboardView.swift
+++ osx/PhotoBook/PhotoBook/DashboardView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  DashboardView.swift
 //  PhotoBook
 //
 //  Created by Cosmin Mihai on 31.08.2023.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct DashboardView: View {
     var buttonBackgroundColor: Color
     var body: some View {
         VStack {
@@ -33,8 +33,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct DashboardView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(buttonBackgroundColor: Color(red:21.6/100, green:26.3/100, blue:27.5/100))
+        DashboardView(buttonBackgroundColor: Color(red:21.6/100, green:26.3/100, blue:27.5/100))
     }
 }
---
+++ osx/PhotoBook/PhotoBook/TableContentView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  PhotoBook
+//
+//  Created by Cosmin Mihai on 29.10.2023.
+//
+
+import SwiftUI
+
+struct TableContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TableContentView()
+}
