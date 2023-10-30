98cd8503602ad68294c969d378813547cfe35eb9
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-10-30 16:24:37 +0200

    dev: Change Exportable parent.

--- PB/include/pb/Exportable.h
+++ PB/include/pb/Exportable.h
@@ -2,17 +2,24 @@
 
 #include <string>
 
+#include <pb/util/Thread.h>
 #include <pb/util/Traits.h>
 
 namespace PB {
-class Exportable {
+class Exportable : public Thread {
 public:
+  Exportable(std::stop_token stopToken) : Thread(stopToken) {}
+  virtual ~Exportable() = default;
+
   void configureExport(std::string name, Path destination,
                        std::vector<Path> images)
   {
     mName = name;
     mDestination = destination;
-    std::copy(images.begin(), images.end(), mImages.begin());
+    for (auto& image : images)
+    {
+      mImages.push_back(image);
+    }
   }
 
 protected:
