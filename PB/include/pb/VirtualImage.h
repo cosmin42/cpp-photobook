1f2f171e134d08e138a8797cbc2ff54b6b2c12b3
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-11-01 18:02:20 +0200

    dev: Add `VirtualImage`.

---
+++ PB/include/pb/VirtualImage.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#pragma warning(push)
+#pragma warning(disable : 5054)
+#pragma warning(disable : 4127)
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#pragma warning(pop)
+
+namespace PB {
+class VirtualImage {
+public:
+  virtual cv::Mat image() = 0;
+};
+} // namespace PB
