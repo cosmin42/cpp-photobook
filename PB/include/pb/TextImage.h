cd3021743a8c44a96f03f80aff1a66b6e2f869a4
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-11-02 17:09:49 +0200

    dev: Add text parameter to `TextImage`.

--- PB/include/pb/TextImage.h
+++ PB/include/pb/TextImage.h
@@ -5,12 +5,15 @@
 namespace PB {
 class TextImage final : public VirtualImage {
 public:
-  TextImage() {}
+  TextImage(std::string text) : mText(text) {}
 
   cv::Mat fullImage() override { return cv::Mat(); }
   cv::Mat mediumImage() override { return cv::Mat(); }
   cv::Mat smallImage() override { return cv::Mat(); }
 
   VirtualImageType type() const override { return VirtualImageType::Text; }
+
+private:
+  std::string mText;
 };
 } // namespace PB
