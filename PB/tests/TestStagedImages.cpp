662ed80ea75c1cfcc4d3244030dcb3de62b49c59
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-12-07 19:44:25 +0200

    tests: Add `StagedImagesListener` manipulation test.

--- PB/tests/TestStagedImages.cpp
+++ PB/tests/TestStagedImages.cpp
@@ -1,5 +1,5 @@
-#include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
 #include <pb/Config.h>
 #include <pb/StagedImages.h>
@@ -24,9 +24,76 @@ TEST(TestStagedImages, CreateStageImages)
       std::make_shared<TestStagedImagesListener>();
 
   stagedImages->setListener(listener);
-
 }
 
+TEST(TestStagedImages, AddPictures)
+{
+  std::shared_ptr<PB::StagedImages> stagedImages =
+      std::make_shared<PB::StagedImages>();
+
+  std::shared_ptr<TestStagedImagesListener> listener =
+      std::make_shared<TestStagedImagesListener>();
+
+  stagedImages->setListener(listener);
+
+  std::vector<std::shared_ptr<PB::VirtualImage>> pictures;
+
+  EXPECT_CALL(*listener.get(), onPicturesAdded(0, 0));
+
+  stagedImages->addPictures(pictures);
+
+  std::shared_ptr<PB::VirtualImage> image0 =
+      std::make_shared<TestStagedImagesVirtualImage>();
+
+  std::shared_ptr<PB::VirtualImage> image1 =
+      std::make_shared<TestStagedImagesVirtualImage>();
+
+  pictures.push_back(image0);
+  pictures.push_back(image1);
+
+  EXPECT_CALL(*listener.get(), onPicturesAdded(0, 2));
+
+  stagedImages->addPictures(pictures);
+
+  auto const &retrievedPictures = stagedImages->stagedPhotos();
+
+  EXPECT_EQ(retrievedPictures.size(), 2);
+
+  std::shared_ptr<PB::VirtualImage> image2 =
+      std::make_shared<TestStagedImagesVirtualImage>();
+  std::shared_ptr<PB::VirtualImage> image3 =
+      std::make_shared<TestStagedImagesVirtualImage>();
+  std::shared_ptr<PB::VirtualImage> image4 =
+      std::make_shared<TestStagedImagesVirtualImage>();
+  std::shared_ptr<PB::VirtualImage> image5 =
+      std::make_shared<TestStagedImagesVirtualImage>();
+  std::shared_ptr<PB::VirtualImage> image6 =
+      std::make_shared<TestStagedImagesVirtualImage>();
+
+  std::vector<std::shared_ptr<PB::VirtualImage>> otherPictures;
+
+  otherPictures.push_back(image2);
+  otherPictures.push_back(image3);
+  otherPictures.push_back(image4);
+  otherPictures.push_back(image5);
+  otherPictures.push_back(image6);
+
+  EXPECT_CALL(*listener.get(), onPicturesAdded(2, 5));
+
+  stagedImages->addPictures(otherPictures);
+
+  EXPECT_CALL(*listener.get(), onPictureRemoved(std::vector<unsigned>{}));
+
+  stagedImages->removePicture({});
+
+  EXPECT_CALL(*listener.get(), onPictureRemoved(std::vector<unsigned>{1}));
+
+  stagedImages->removePicture({1});
+
+  EXPECT_CALL(*listener.get(), onPictureRemoved(std::vector<unsigned>{0, 1, 2, 3, 4, 5}));
+
+  stagedImages->clear();
+}
 
 int main(int argc, char **argv)
 {
