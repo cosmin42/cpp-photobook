1c4ebb9e8b8b1abf2457af4bf49368daa6669a19
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-12-07 18:48:17 +0200

    dev: Add .

--- PB/include/pb/StagedImages.h
+++ PB/include/pb/StagedImages.h
@@ -1,5 +1,32 @@
 #pragma once
 
+#include <memory>
+#include <vector>
+
+#include <pb/VirtualImage.h>
+
 namespace PB {
 
-}
+class StagedImagesListener {
+public:
+  virtual void onPicturesAdded(int index, int size) = 0;
+  virtual void onPictureRemoved(std::vector<unsigned> index) = 0;
+};
+
+class StagedImages final {
+public:
+  void setListener(std::shared_ptr<StagedImagesListener> listener);
+
+  void addPicture(std::vector<std::shared_ptr<VirtualImage>> pictures);
+  void addPicture(std::vector<std::shared_ptr<VirtualImage>> pictures,
+                  int                                        position);
+  void removePicture(std::vector<unsigned> indexes);
+
+  std::vector<std::shared_ptr<VirtualImage>> const &stagedPhotos() const;
+  std::shared_ptr<VirtualImage> picture(unsigned index) const;
+
+private:
+  std::shared_ptr<StagedImagesListener>      mListener;
+  std::vector<std::shared_ptr<VirtualImage>> mStagedPhotos;
+};
+} // namespace PB
--- PB/src/StagedImages.cpp
+++ PB/src/StagedImages.cpp
@@ -2,4 +2,58 @@
 
 namespace PB {
 
+void StagedImages::setListener(std::shared_ptr<StagedImagesListener> listener)
+{
+  mListener = listener;
 }
+
+void StagedImages::addPicture(
+    std::vector<std::shared_ptr<VirtualImage>> pictures)
+{
+  int nextIndex = (int)mStagedPhotos.size();
+  mStagedPhotos.insert(mStagedPhotos.end(), pictures.begin(), pictures.end());
+
+  PB::basicAssert(mListener != nullptr);
+  mListener->onPicturesAdded(nextIndex, pictures.size());
+}
+
+void StagedImages::addPicture(
+    std::vector<std::shared_ptr<VirtualImage>> pictures, int position)
+{
+  PB::basicAssert(position > -1);
+  PB::basicAssert(position <= (int)mStagedPhotos.size());
+
+  if (position < (int)mStagedPhotos.size()) {
+    mStagedPhotos.insert(mStagedPhotos.begin() + position, pictures.begin(),
+                         pictures.end());
+  }
+  else {
+    mStagedPhotos.insert(mStagedPhotos.end(), pictures.begin(), pictures.end());
+  }
+
+  PB::basicAssert(mListener != nullptr);
+  mListener->onPicturesAdded(position, pictures.size());
+}
+
+void StagedImages::removePicture(std::vector<unsigned> indexes)
+{
+  std::sort(indexes.begin(), indexes.end(), std::greater<int>());
+  for (int i = 0; i < indexes.size(); ++i) {
+    mStagedPhotos.erase(mStagedPhotos.begin() + indexes.at(i));
+  }
+  mListener->onPictureRemoved(indexes);
+}
+
+std::vector<std::shared_ptr<VirtualImage>> const &
+StagedImages::stagedPhotos() const
+{
+  return mStagedPhotos;
+}
+
+std::shared_ptr<VirtualImage> StagedImages::picture(unsigned index) const
+{
+  PB::basicAssert(index < mStagedPhotos.size());
+  return mStagedPhotos.at(index);
+}
+
+} // namespace PB
