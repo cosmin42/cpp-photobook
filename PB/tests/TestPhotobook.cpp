07d6895e73e69b64f817775dd478673bd6aff63d
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-12-14 22:30:37 +0200

    tests: Add tests configuration.

--- PB/tests/TestPhotobook.cpp
+++ PB/tests/TestPhotobook.cpp
@@ -1,5 +1,32 @@
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include <pb/PhotoBook.h>
 
-TEST(TestPhotobook, TestCreation) { PB::Photobook photobook("."); }
+class TestPhotobookStagedImagesListener : public PB::StagedImagesListener {
+public:
+  MOCK_METHOD(void, onPicturesAdded, (int index, int size), (override));
+  MOCK_METHOD(void, onPictureRemoved, (std::vector<unsigned> index),
+              (override));
+};
+
+class MockPhotobookImageMonitorListener final
+    : public PB::ImageMonitorListener {
+public:
+  MOCK_METHOD(void, onImportFolderAdded, (), (override));
+  MOCK_METHOD(void, onImportFolderRemoved, (unsigned index), (override));
+  MOCK_METHOD(void, onCleared, (), (override));
+};
+
+TEST(TestPhotobook, TestCreation)
+{
+  std::shared_ptr<PB::StagedImagesListener> stagedImageListener =
+      std::make_shared<TestPhotobookStagedImagesListener>();
+
+  std::shared_ptr<PB::ImageMonitorListener> imageMonitorListener =
+      std::make_shared<MockPhotobookImageMonitorListener>();
+
+  PB::Photobook photobook(".");
+  photobook.configure(stagedImageListener.get());
+  photobook.configure(imageMonitorListener.get());
+}
