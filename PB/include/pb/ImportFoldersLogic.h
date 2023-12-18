7190cbd9767f420dbe0b159ea7a61287d1d93f7f
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2023-12-18 14:41:02 +0200

    dev: Add `ImportFoldersLogic` implementation.

--- PB/include/pb/ImportFoldersLogic.h
+++ PB/include/pb/ImportFoldersLogic.h
@@ -1,9 +1,56 @@
 #pragma once
 
-namespace PB
-{
-class ImportFoldersLogic
-{
+#include <unordered_map>
+
+#include <pb/Config.h>
+#include <pb/FileMapper.h>
+#include <pb/ThumbnailsProcessor.h>
+
+namespace PB {
+
+class ImportFoldersLogicListener {
 public:
+  virtual ~ImportFoldersLogicListener() = default;
+
+  virtual void onMapped(Path, std::vector<Path> newFolders) = 0;
+  virtual void onImportStop(Path) = 0;
+
+  virtual void onImageProcessed(Path root, Path full, Path medium, Path small,
+                                int progress, int progressCap) = 0;
+
+  virtual void onMappingAborted(Path) = 0;
 };
-}
+
+class ThreadScheduler {
+public:
+  virtual void post(std::function<void()> f) = 0;
+};
+
+class ImportFoldersLogic final : public Observer {
+public:
+  void configure(ImportFoldersLogicListener *listener);
+  void configure(ThreadScheduler *scheduler);
+  void configure(std::pair<int, int> screenSize);
+  void configure(ProjectSnapshot project);
+
+  std::optional<Error> addImportFolder(Path path);
+
+  void start(Path path);
+  void stop(Path path);
+  void stopAll();
+  void update(ObservableSubject &subject);
+
+private:
+  void setObserverManager();
+
+  void processImages(Path root, std::vector<Path> newFolders);
+
+  void onImageProcessed(Path root, Path full, Path medium, Path small,
+                        int progress, int progressCap);
+
+  ImportFoldersLogicListener           *mListener = nullptr;
+  ThreadScheduler                      *mScheduler = nullptr;
+  std::unordered_map<Path, MediaMapper> mMappingJobs;
+  ThumbnailsProcessor                   mThumbnailsProcessor;
+};
+} // namespace PB
--- PB/src/ImportFoldersLogic.cpp
+++ PB/src/ImportFoldersLogic.cpp
@@ -1,6 +1,119 @@
 #include <pb/ImportFoldersLogic.h>
 
-namespace PB
+#include <pb/util/FileInfo.h>
+
+namespace PB {
+void ImportFoldersLogic::configure(ImportFoldersLogicListener *listener)
 {
-    
+  mListener = listener;
 }
+
+void ImportFoldersLogic::configure(ThreadScheduler *scheduler)
+{
+  mScheduler = scheduler;
+}
+
+void ImportFoldersLogic::configure(std::pair<int, int> screenSize) {
+  mThumbnailsProcessor.setScreenSize(screenSize);
+}
+void ImportFoldersLogic::configure(ProjectSnapshot project) {
+  mThumbnailsProcessor.provideProjectDetails(project);
+}
+
+void ImportFoldersLogic::setObserverManager()
+{
+  setChangeFunction([this](PB::Observer *current, PB::Observer *other) {
+    if (other) {
+      for (auto &[key, value] : mMappingJobs) {
+        value.dettach(current);
+        value.attach(other);
+      }
+    }
+    else {
+      PB::basicAssert(false);
+    }
+  });
+}
+
+std::optional<Error> ImportFoldersLogic::addImportFolder(Path path)
+{
+  auto errorOrPath = FileInfo::validInputRootPath(path);
+  if (std::holds_alternative<Error>(errorOrPath)) {
+    return std::get<Error>(errorOrPath);
+  }
+
+  setObserverManager();
+
+  auto mapper = MediaMapper(path);
+  mapper.attach(this);
+  mMappingJobs.emplace(path, mapper);
+
+  return std::nullopt;
+}
+
+void ImportFoldersLogic::start(Path path) { mMappingJobs.at(path).start(); }
+
+void ImportFoldersLogic::stop(Path path) {}
+
+void ImportFoldersLogic::stopAll() {}
+
+void ImportFoldersLogic::update(ObservableSubject &subject)
+{
+  if (dynamic_cast<MediaMapper *>(&subject) != nullptr) {
+    auto &mediaMap = static_cast<MediaMapper &>(subject);
+    if (mediaMap.state() == MediaMapState::Finished) {
+      mListener->onMapped(mediaMap.root(), mediaMap.importedDirectories());
+    }
+    else if (mediaMap.state() == MediaMapState::Aborted) {
+      mListener->onMappingAborted(mediaMap.root());
+    }
+  }
+  else {
+    PB::basicAssert(false);
+  }
+}
+
+void ImportFoldersLogic::onImageProcessed(Path root, Path full, Path medium,
+                                          Path small, int progress,
+                                          int progressCap)
+{
+
+  mScheduler->post([this, root, full, medium, small, progress, progressCap]() {
+    mListener->onImageProcessed(root, full, medium, small, progress, progressCap);
+
+  });
+
+  /*
+  mParent->post(
+      [this, root{root}, input{input}, smallOutput{smallOutput}, start{start},
+       mediumOutput{mediumOutput}, maxProgress{maxProgress}]() {
+        auto image = mImageSupport.image<VirtualImage>(input);
+
+        image->setMediumSizePath(mediumOutput);
+        image->setSmallSizePath(smallOutput);
+
+        mImageSupport.updateStagedPhoto(image);
+
+        mProgress[rootPath]++;
+        if (mProgress.at(rootPath) == maxProgress) {
+
+          auto end = std::chrono::high_resolution_clock::now();
+          std::chrono::duration<double> duration = end - start;
+          PB::printDebug("Duration: %f\n", duration.count());
+
+          mParent->onFinished(rootPath);
+          mMappingJobs.erase(rootPath);
+        }
+        */
+}
+
+void ImportFoldersLogic::processImages(Path root, std::vector<Path> newFolders)
+{
+  mThumbnailsProcessor.generateThumbnails(
+      newFolders, root.string(),
+      [this, root{root}, start{start}, maxProgress{newFolders.size()}](
+          Path full, Path medium, Path small, int position) {
+        onImageProcessed(root, full, medium, small, position, maxProgress);
+      });
+}
+} // namespace PB
