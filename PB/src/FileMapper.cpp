2b8564372c21a4625265d590567bf240058258d5
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2024-01-28 11:57:45 +0200

    dev: Add file filtering on mapping.

--- PB/src/FileMapper.cpp
+++ PB/src/FileMapper.cpp
@@ -1,3 +1,4 @@
+#include <pb/image/ImageOperations.h>
 #include <pb/tasks/FileMapper.h>
 
 namespace PB {
@@ -12,11 +13,19 @@ void MediaMapper::taskStep()
 {
   Path path = mRecursiveIterator->path();
 
+  if (std::filesystem::is_regular_file(path) && Process::validExtension(path)) {
+    mSubFiles.push_back(path);
+  }
+
+  if (std::filesystem::is_directory(path)) {
+    mSubFiles.push_back(path);
+  }
+
+  mRecursiveIterator++;
+
 #ifdef SIMULATE_SLOW_MAPPER
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 #endif
-  mSubFiles.push_back(path);
-  mRecursiveIterator++;
 }
 
 bool MediaMapper::stoppingCondition() const
