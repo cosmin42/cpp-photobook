05e4dc98a7eaa06dfd30458eb6963683c83dc8dc
Cosmin Mihai <67975221+cosmin42@users.noreply.github.com>
2024-02-02 10:07:14 +0200

    Add makeName draft #36

--- PB/src/Jpg.cpp
+++ PB/src/Jpg.cpp
@@ -7,4 +7,23 @@ bool JpgExport::stoppingCondition() const { return true; }
 int JpgExport::stepsCount() const { return 0; }
 
 void JpgExport::taskStep() {}
+/*
+std::string makeName(unsigned counter)
+{
+    struct JPG_TEMPLATE_PARAMS
+    {
+        static constexpr const char* JPG_NAME_TEMPLATE = "PHOTO_0000000.JPG";
+        static constexpr unsigned PLACEHOLDER_SIZE = 7;
+        static constexpr unsigned PLACEHOLDER_OFFSET = 6;
+    };
+    
+    auto counterStr = std::to_string(counter);
+    PBDev::basicAssert(counterStr.size() <= JPG_TEMPLATE_PARAMS::PLACEHOLDER_SIZE);
+    std::string result = JPG_TEMPLATE_PARAMS::JPG_NAME_TEMPLATE;
+    unsigned replaceOffset = JPG_TEMPLATE_PARAMS::PLACEHOLDER_OFFSET + (JPG_TEMPLATE_PARAMS::PLACEHOLDER_SIZE-counterStr.length());
+    result.replace(replaceOffset, counterStr.size(), counterStr);
+
+    return result;
+}
+*/
 } // namespace PB
