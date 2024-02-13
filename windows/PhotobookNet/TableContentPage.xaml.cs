d467450fd784290ff0c48c602fb94bbbca340b5a
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2024-02-13 16:40:29 +0200

    build: Update nuget packages, add Win2D.

--- windows/PhotobookNet/TableContentPage.xaml.cs
+++ windows/PhotobookNet/TableContentPage.xaml.cs
@@ -1,17 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -65,14 +56,8 @@ namespace PhotobookNet
         private async void OnGalleryLeft(object sender, RoutedEventArgs args) { }
 
         private async void OnGalleryRight(object sender, RoutedEventArgs args) { }
-        /*
-        void OnCanvasDraw([[maybe_unused]] winrt::Microsoft::Graphics::Canvas::UI::
-                        Xaml::CanvasControl sender,
-
-
-                          [[maybe_unused]] winrt::Microsoft::Graphics::Canvas::UI::
-                        Xaml::CanvasDrawEventArgs args);
-        */
+        //private async void OnCanvasDraw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args);
+        
         /* Book Lines */
 
         private async void OnNavigatedTo(NavigationEventArgs args) { }
