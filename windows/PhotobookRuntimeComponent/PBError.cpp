186ab7ca4bb4917e8ed8b888c0b65effa083dbc2
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2024-03-09 14:26:43 +0200

    [dev][MIDL] Add `PBError`.

---
+++ windows/PhotobookRuntimeComponent/PBError.cpp
@@ -0,0 +1,2 @@
+#include "pch.h"
+#include "PBError.h"
---
+++ windows/PhotobookRuntimeComponent/PBError.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "PBError.g.h"
+
+#include <pb/util/Error.h>
+
+namespace winrt::PhotobookRuntimeComponent::implementation {
+struct PBError : PBErrorT<PBError> {
+  PBError() {}
+  explicit PBError(PBDev::Error error) : mError(error) {}
+
+  winrt::hstring description()
+  {
+    return winrt::to_hstring(mError.description());
+  }
+  PBErrorCode errorCode() { return (PBErrorCode)mError.kind(); }
+
+private:
+  PBDev::Error mError;
+};
+} // namespace winrt::PhotobookRuntimeComponent::implementation
