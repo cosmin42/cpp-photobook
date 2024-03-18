3ccf68fce98ba0e4f830e87d2c05cd1e62a5d766
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2024-03-18 20:58:54 +0200

    ui: Instantiate `PhotobookWin`.

--- windows/PhotobookNet/PhotobookSingletonWrapper.cs
+++ windows/PhotobookNet/PhotobookSingletonWrapper.cs
@@ -4,17 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PhotobookRuntimeComponent;
+using Windows.Gaming.XboxLive;
 
 namespace PhotobookNet
 {
     class PhotobookSingletonWrapper
     {
-        private static PhotobookWin instance;
+        private static PhotobookWin instance = null;
 
         private PhotobookSingletonWrapper() { }
 
         public static PhotobookWin GetInstance()
         {
+            if (instance == null)
+            {
+                var localFolderPath = Windows.Storage.ApplicationData.Current.LocalFolder.Path;
+                var installFolderPath = Windows.ApplicationModel.Package.Current.InstalledLocation.Path;
+                instance = new PhotobookWin(localFolderPath.ToString(), installFolderPath.ToString());
+            }
             return instance;
         }
     }
