698566096e1bf6588a921c3078fb4d06e9e195cc
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2024-05-11 11:57:44 +0300

    Add `MapReducer`.

---
+++ PB/include/pb/MapReducer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <functional>
+#include <optional>
+
+#include <boost/uuid/uuid.hpp>
+
+namespace PB {
+typedef std::pair<boost::uuids::uuid, std::function<void()>>
+    IdentifyableFunction;
+
+class MapReducer {
+public:
+  MapReducer() = default;
+  virtual ~MapReducer() = default;
+
+  virtual std::optional<IdentifyableFunction> getNext() = 0;
+
+  virtual void onFinished(const boost::uuids::uuid id) = 0;
+};
+} // namespace PB
