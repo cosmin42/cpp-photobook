cea95ea125aef6b929cd180b0a12b46c42601530
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2024-05-11 15:16:55 +0300

    dev: Add crunch method.

--- PB/include/pb/TaskCruncher.h
+++ PB/include/pb/TaskCruncher.h
@@ -4,6 +4,8 @@
 
 #include <pb/tasks/ParallelTaskConsumer.h>
 
+#include <pb/MapReducer.h>
+
 namespace PB
 {
 class TaskCruncher final
@@ -17,6 +19,8 @@ public:
   TaskCruncher(TaskCruncher&&) = delete;
 
   void registerPTC(const std::string name);
+
+  void crunch(const std::string, MapReducer &mapper);
 
   private:
   std::unordered_map<std::string, PBDev::ParallelTaskConsumer> mPTC;
--- PB/src/TaskCruncher.cpp
+++ PB/src/TaskCruncher.cpp
@@ -1,5 +1,23 @@
 #include <pb/TaskCruncher.h>
 
+#include <pb/RuntimeUUID.h>
+
 namespace PB {
 void TaskCruncher::registerPTC(const std::string name) { mPTC[name]; }
+
+void TaskCruncher::crunch(const std::string name, MapReducer &mapper)
+{
+  auto task = mapper.getNext();
+  while (task.has_value()) {
+    auto uuid = RuntimeUUID::newUUID();
+
+    mPTC.at(name).enqueue(uuid, [task{task}, &mapper]() {
+      task->second();
+      mapper.onFinished(task->first);
+    });
+
+    task = mapper.getNext();
+  }
+}
+
 } // namespace PB
