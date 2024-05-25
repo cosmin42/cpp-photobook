63009c010cb9c95beb7623281e6bd8a3b3a93cc1
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2024-05-25 20:41:57 +0300

    dev: Change `Context` to `OneConfig`.

--- PB/src/Config.cpp
+++ PB/src/Config.cpp
@@ -2,6 +2,11 @@
 
 #include <source_location>
 
+#pragma warning(push)
+#pragma warning(disable : 4996)
+#include <spdlog/spdlog.h>
+#pragma warning(pop)
+
 namespace PB {
 Context Context::sContext;
 }
@@ -10,6 +15,12 @@ namespace PBDev
 {
 void basicAssert(int shouldBetrue, const std::source_location location)
 {
+  if (!shouldBetrue) {
+    std::string message = std::string(location.function_name()) + ":" +
+                          std::to_string(location.line());
+
+    spdlog::info("{}\n", message);
+  }
 #ifdef _DEBUG
   assert(shouldBetrue);
 #else
