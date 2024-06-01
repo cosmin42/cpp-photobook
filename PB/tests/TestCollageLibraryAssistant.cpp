f3f15e2e44f2d68db0d37829815aa95c4ecbced2
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2024-06-01 13:30:03 +0300

    tests: Add CollageLibraryAssistant test.

--- PB/tests/TestCollageLibraryAssistant.cpp
+++ PB/tests/TestCollageLibraryAssistant.cpp
@@ -6,7 +6,25 @@ using namespace PB;
 
 TEST(TestCollageAssistant, ExtractProfile)
 {
-  CollageLibraryAssistant assistant(".", ".");
+  CollageLibraryAssistant assistant(std::filesystem::current_path(),
+                                    std::filesystem::current_path());
 
   auto paths = assistant.createNumberedImages({2480, 1754});
+
+  EXPECT_EQ(paths.size(), 10);
+
+  AspectRatio aspectRatio = {4, 3};
+
+  auto templatePath = std::filesystem::current_path() / Path("svg-templates") /
+                      "2x1-simple.svg.template";
+
+  assistant.createTemplateThumbnail(templatePath, aspectRatio, {2480, 1754});
+
+  for (auto i = 0; i < 10; ++i) {
+    std::filesystem::remove(std::filesystem::current_path() /
+                            (std::string("placeolder_") + std::to_string(i)));
+  }
+
+  std::filesystem::remove(std::filesystem::current_path() /
+                          "template_4to3-2480x1754.svg");
 }
