89ed0cdfe99c69f76c742ee65022f2aed1279991
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2024-06-09 17:49:29 +0300

    build: Add Photobook headers.

---
+++ osx/PhotoBook/PhotoBook/PhotoBook-Bridging-Header.mm
@@ -0,0 +1,10 @@
+//
+//  PhotoBook-Bridging-Header.m
+//  PhotoBook
+//
+//  Created by Cosmin Mihai on 09.06.2024.
+//
+
+#import "PhotoBook-Bridging-Header.h"
+
+#include <pb/util/Traits.h>
---
+++ osx/PhotoBook/PhotoBook/Photobook.m
@@ -0,0 +1,8 @@
+//
+//  Photobook.m
+//  PhotoBook
+//
+//  Created by Cosmin Mihai on 09.06.2024.
+//
+
+#import <Foundation/Foundation.h>
