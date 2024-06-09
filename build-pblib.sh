1d1a1456bc2866e84eb7086750577b7fc7b56d69
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2024-06-09 16:48:07 +0300

    build: Remove isysroot.

--- build-pblib.sh
+++ build-pblib.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-cmake -B PB/build -S PB -DCMAKE_C_COMPILER=/usr/local/bin/clang -DCMAKE_CXX_COMPILER=/usr/local/bin/clang++  -DCMAKE_CXX_FLAGS=`-isysroot $(xcrun --show-sdk-path)` -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake -B PB/build -S PB -DCMAKE_C_COMPILER=/usr/local/bin/clang -DCMAKE_CXX_COMPILER=/usr/local/bin/clang++  -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
