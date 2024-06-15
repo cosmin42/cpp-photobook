ed991d2dc450e25a8098ed97cc7b963c7b8e6304
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2024-06-15 17:06:24 +0300

    The NSString problem.

--- osx/PhotoBook/PhotoBook/Photobook.h
+++ osx/PhotoBook/PhotoBook/Photobook.h
@@ -8,6 +8,8 @@
 #ifndef Photobook_h
 #define Photobook_h
 
+#import <Foundation/Foundation.h>
+
 @interface PhotobookListenerWrapperCLevel: NSObject
 - (void)onProjectRead;
 @end
@@ -15,6 +17,11 @@
 @interface Photobook : NSObject
 - (id)init;
 - (void) setPhotobookListener:PhotobookListenerWrapperCLevel;
+@end
+
+@interface XC: NSObject
+- (NSString *)concatenateString:(NSString *)str1 withString:(NSString *)str2;
+- (void) g;
 @end
 
 #endif /* Photobook_h */
