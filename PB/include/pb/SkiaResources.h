2a655fdecb3125f82993c5cbfbfd9c12a02ae8d9
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2024-06-23 19:04:06 +0300

    dev: Add `SkiaResources`.

--- PB/include/pb/SkiaResources.h
+++ PB/include/pb/SkiaResources.h
@@ -1,8 +1,31 @@
 #pragma once
 
-namespace PB
-{
-class SkiaResources
-{
+#include <unordered_map>
+
+#include <modules/skresources/include/SkResources.h>
+
+#include <pb/util/Traits.h>
+
+DECLARE_STRONG_UUID(SkiaResourcesId)
+
+namespace PB {
+class SkiaResources final {
+public:
+  SkiaResources() = default;
+  ~SkiaResources() = default;
+
+  PBDev::SkiaResourcesId addResource(Path path);
+  void                   removeResource(PBDev::SkiaResourcesId id);
+
+  sk_sp<skresources::FileResourceProvider>
+  resourceProvider(PBDev::SkiaResourcesId id);
+
+private:
+  SkiaResources(const SkiaResources &) = delete;
+  SkiaResources &operator=(const SkiaResources &) = delete;
+
+  std::unordered_map<PBDev::SkiaResourcesId,
+                     sk_sp<skresources::FileResourceProvider>>
+      mResourceProviders;
 };
-}
+} // namespace PB
--- PB/src/SkiaResources.cpp
+++ PB/src/SkiaResources.cpp
@@ -1,6 +1,28 @@
 #include <pb/SkiaResources.h>
 
+#include <src/utils/SkOSPath.h>
 
-namespace PB
+namespace PB {
+
+PBDev::SkiaResourcesId SkiaResources::addResource(Path path)
 {
+  auto assetPath = SkOSPath::Dirname(path.string().c_str());
+
+  auto provider = skresources::FileResourceProvider::Make(assetPath);
+  PBDev::SkiaResourcesId id(RuntimeUUID::newUUID());
+  mResourceProviders[id] = provider;
+  return id;
 }
+
+void SkiaResources::removeResource(PBDev::SkiaResourcesId id)
+{
+  mResourceProviders.erase(id);
+}
+
+sk_sp<skresources::FileResourceProvider>
+SkiaResources::resourceProvider(PBDev::SkiaResourcesId id)
+{
+  return mResourceProviders.at(id);
+}
+
+} // namespace PB
