a6fda103397b98608e7ce34a627c9ea89aa63f86
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2024-07-14 15:43:00 +0300

    ui: Change menu bar color.

--- windows/PhotobookNet/MainWindow.xaml.cs
+++ windows/PhotobookNet/MainWindow.xaml.cs
@@ -1,16 +1,11 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Input;
 using System;
-using System.Collections.Generic;
-using PhotobookRuntimeComponent;
-using Windows.Foundation.Collections;
-using System.Collections.ObjectModel;
-using WinRT;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.Graphics.Canvas.Effects;
 using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.UI;
+using System.Diagnostics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -22,11 +17,18 @@ namespace PhotobookNet
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private readonly AppWindow mAppWindow = null;
+
         public MainWindow()
         {
             this.InitializeComponent();
 
             Frame rootFrame = MainFrame;
+
+            mAppWindow = AppWindow;
+            bool success = SetTitleBarColors();
+
+            Debug.Assert(success, "SetTitleBarColors failed");
 
             var windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(this);
 
@@ -41,6 +43,39 @@ namespace PhotobookNet
         private void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new NotImplementedException();
+        }
+
+        private bool SetTitleBarColors()
+        {
+            // Check to see if customization is supported.
+            // The method returns true on Windows 10 since Windows App SDK 1.2,
+            // and on all versions of Windows App SDK on Windows 11.
+            if (AppWindowTitleBar.IsCustomizationSupported())
+            {
+                AppWindowTitleBar m_TitleBar = mAppWindow.TitleBar;
+
+                // Set active window colors.
+                // Note: No effect when app is running on Windows 10
+                // because color customization is not supported.
+                m_TitleBar.ForegroundColor = Colors.White;
+                m_TitleBar.BackgroundColor = Color.FromArgb(0xFF, 0x1A, 0x22, 0x24);
+                m_TitleBar.ButtonForegroundColor = Colors.White;
+                m_TitleBar.ButtonBackgroundColor = Color.FromArgb(0xFF, 0x1A, 0x22, 0x24);
+                m_TitleBar.ButtonHoverForegroundColor = Colors.Gainsboro;
+                m_TitleBar.ButtonHoverBackgroundColor = Color.FromArgb(0xFF, 0x1A, 0x22, 0x24);
+                m_TitleBar.ButtonPressedForegroundColor = Colors.Gray;
+                m_TitleBar.ButtonPressedBackgroundColor = Color.FromArgb(0xFF, 0x1A, 0x22, 0x24);
+
+                // Set inactive window colors.
+                // Note: No effect when app is running on Windows 10
+                // because color customization is not supported.
+                m_TitleBar.InactiveForegroundColor = Colors.Gainsboro;
+                m_TitleBar.InactiveBackgroundColor = Color.FromArgb(0xFF, 0x1A, 0x22, 0x24);
+                m_TitleBar.ButtonInactiveForegroundColor = Colors.Gainsboro;
+                m_TitleBar.ButtonInactiveBackgroundColor = Color.FromArgb(0xFF, 0x1A, 0x22, 0x24);
+                return true;
+            }
+            return false;
         }
     }
 }
