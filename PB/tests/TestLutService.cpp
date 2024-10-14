e5719b4334fc93fb954be6a1e1547d81eaecd7cf
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2024-10-14 22:16:44 +0300

    dev: Add the thread scheduler mock.

--- PB/tests/TestLutService.cpp
+++ PB/tests/TestLutService.cpp
@@ -2,22 +2,48 @@
 
 #include "MockListeners.h"
 
+#include <pb/components/TSQueue.h>
 #include <pb/services/LutService.h>
-#
+
 using namespace PB;
+
+class ThreadSchedulerMock final : public PBDev::ThreadScheduler {
+public:
+  ~ThreadSchedulerMock() = default;
+
+  void post(std::function<void()> f) override { f(); }
+
+  void mainloop()
+  {
+    while (true) {
+      std::function<void()> f;
+      auto                  f = mQueue.dequeue(std::chrono::milliseconds(2000));
+      if (f) {
+        f();
+      }
+      else {
+        throw;
+      }
+    }
+  }
+
+private:
+  TSQueue<std::function<void()>> mQueue;
+};
 
 TEST(TestLutService, TestEmpty)
 {
-  // mLutService->configureThreadScheduler(threadScheduler);
-
   ProgressServiceListenerTest *progressServiceListener =
       new ProgressServiceListenerTest();
 
   TestLutServiceListener *testLutServiceListener = new TestLutServiceListener();
 
+  ThreadSchedulerMock *threadScheduler = new ThreadSchedulerMock();
+
   std::shared_ptr<ProgressService> progressService =
       std::make_shared<ProgressService>();
   progressService->configure(progressServiceListener);
+  progressService->configureScheduler(threadScheduler);
 
   std::shared_ptr<TaskCruncher> taskCruncher = std::make_shared<TaskCruncher>();
   taskCruncher->configureProgressService(progressService);
@@ -31,13 +57,14 @@ TEST(TestLutService, TestEmpty)
   mLutService->configurePlatformInfo(platformInfo);
   mLutService->configureTaskCruncher(taskCruncher);
   mLutService->configureOGLEngine(mOGLEngine);
+  mLutService->configureThreadScheduler(threadScheduler);
 
   std::stop_source stopSource;
 
   mOGLEngine->start(stopSource.get_token());
   mLutService->startLutService();
 
-
+  delete threadScheduler;
   delete testLutServiceListener;
   delete progressServiceListener;
 }
