c0bf729d83b71ac2b71b2df5d0d79ae855bb1eda
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2024-10-21 22:02:51 +0300

    build: Change build type to debug on macos.

--- build-pblib.sh
+++ build-pblib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cmake -B PB/build -S PB  -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake -B PB/build -S PB -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
