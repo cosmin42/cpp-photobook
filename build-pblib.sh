ef062858cbd786c5f0f87a8010dc8808823ed40d
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2024-11-11 23:50:58 +0200

    build-macos: Add iOS prerequisites and pblib target.

--- build-pblib.sh
+++ build-pblib.sh
@@ -2,3 +2,5 @@
 cmake -B PB/build-x86_64 -DCMAKE_OSX_ARCHITECTURES="x86_64" -S PB -DCMAKE_BUILD_TYPE=Debug -DVCPKG_TARGET_TRIPLET=x64-osx -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
 
 cmake -B PB/build-arm64 -DCMAKE_OSX_ARCHITECTURES="arm64" -S PB -DCMAKE_BUILD_TYPE=Debug -DVCPKG_TARGET_TRIPLET=arm64-osx -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
+
+cmake -B PB/build-ios-arm64 -DCMAKE_OSX_ARCHITECTURES="arm64" -S PB -DCMAKE_BUILD_TYPE=Debug -DVCPKG_TARGET_TRIPLET=arm64-ios -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
