06aa63d285b3ae4e746742983bd667d5ec163049
Cosmin Mihai <ionut.cosmin.mihai@gmail.com>
2024-11-27 23:29:57 +0200

    build-macos: Improve the script that create the fat libraries.

--- generage-lipo-commands.py
+++ generage-lipo-commands.py
@@ -16,6 +16,26 @@ def list_common_files(folder1, folder2):
 
     return (common_files, different_files)
 
+
+def get_lipo_commands(folder1, folder2, dstFolder):
+    (common_files, different_files) = list_common_files(folder1, folder2)
+
+    if len(different_files) > 0:
+        exit(1)
+
+    if common_files:
+        command_list = []
+        for file in common_files:
+            command_content = ("lipo -create -output " + os.path.join(dstFolder, (file.replace(".a", "") + "-macos.a")) + " ")
+            command_content = command_content + (" " + os.path.join(folder1, file) + " ")
+            command_content = command_content + (" " + os.path.join(folder2, file))
+
+            command_list.append(command_content)
+        return command_list
+    else:
+        raise
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="List files with the same names in two folders.")
     parser.add_argument("folder1", type=str, help="Path to the first folder")
@@ -24,24 +44,17 @@ if __name__ == "__main__":
     
 
     args = parser.parse_args()
+    command_list = get_lipo_commands(args.folder1, args.folder2, args.folder3)
 
-    (common_files, different_files) = list_common_files(args.folder1, args.folder2)
+    print("mkdir pb/build")
+    os.system("mkdir pb/build")
 
-    if len(different_files) > 0:
-        exit(1)
+    for command_content in command_list:
+        print(command_content)
+        os.system(command_content)
 
-    if common_files:
-        for file in common_files:
-            print("lipo -create -output " + os.path.join(args.folder3, (file.replace(".a", "") + "-macos.a")) + " \\")
-            print("    " + os.path.join(args.folder1, file) + " \\")
-            print("    " + os.path.join(args.folder2, file))
-            print()
+    main_command = get_lipo_commands("pb/x64-macos", "pb/arm64-macos", args.folder3)
 
-        print("mkdir pb/build")
-
-        print("lipo -create -output pb/build/libpblib-macos.a \\")
-        print("    pb/x64-macos/libpblib-Darwin.a \\")
-        print("    pb/arm64-macos/libpblib-Darwin.a")
-
-    else:
-        print("No common files found.")
+    print(main_command[0])
+    os.system(main_command[0])
+    
