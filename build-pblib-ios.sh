fda8d21c09fa39e4b69279625264b4e3b9b84f4b
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2024-12-15 13:20:31 +0200

    build: Split ios-macos build.

---
+++ build-pblib-ios.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cmake -B PB/arm64-ios \
+    -DCMAKE_OSX_ARCHITECTURES="arm64" \
+    -DCMAKE_SYSTEM_NAME="iOS" \
+    -S PB \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DVCPKG_TARGET_TRIPLET=arm64-ios \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=18.1 \
+    -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
+
+make -j4 -C pb/arm64-ios/
--- build-pblib.sh
+++ build-pblib.sh
@@ -15,15 +15,5 @@ cmake -B PB/arm64-macos \
     -DVCPKG_TARGET_TRIPLET=arm64-osx \
     -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
 
-cmake -B PB/arm64-ios \
-    -DCMAKE_OSX_ARCHITECTURES="arm64" \
-    -DCMAKE_SYSTEM_NAME="iOS" \
-    -S PB \
-    -DCMAKE_BUILD_TYPE=Debug \
-    -DVCPKG_TARGET_TRIPLET=arm64-ios \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=18.1 \
-    -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
-
 make -j4 -C pb/x64-macos/ pblib-Darwin
 make -j4 -C pb/arm64-macos/ pblib-Darwin
-make -j4 -C pb/arm64-ios/
