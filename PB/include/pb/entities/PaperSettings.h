f747908c0ab1bff92d27fca2f6dbcb1080794579
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2024-12-18 19:10:05 +0200

    dev: Add PaperSettings.

--- PB/include/pb/entities/PaperSettings.h
+++ PB/include/pb/entities/PaperSettings.h
@@ -2,6 +2,8 @@
 
 #include <string>
 
+#include <magic_enum.hpp>
+
 #include <pb/Enums.h>
 
 namespace PB {
@@ -10,6 +12,13 @@ struct PaperSettings {
   int       ppi = 300;
   int       width = 3508;
   int       height = 2480;
+
+  // add to string method
+  operator std::string() const
+  {
+    return std::to_string(width) + "x" + std::to_string(height) + " " +
+           std::to_string(ppi) + "ppi " + std::string(magic_enum::enum_name(type));
+  }
 };
 
 PaperType deserializePaperType(std::string paperType);
