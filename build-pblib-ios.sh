3df70e23db95b7cd92474caf2b86f3602108d800
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2024-12-28 18:59:21 +0200

    Revert "fix: Fix vcpkg path."
    This reverts commit 469f41c7631a45d83ef597453b507ca1b8627adc.

--- build-pblib-ios.sh
+++ build-pblib-ios.sh
@@ -7,6 +7,6 @@ cmake -B PB/arm64-ios \
     -DCMAKE_BUILD_TYPE=Debug \
     -DVCPKG_TARGET_TRIPLET=arm64-ios \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=18.1 \
-    -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
+    -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
 
 make -j4 -C pb/arm64-ios/
--- build-pblib.bat
+++ build-pblib.bat
@@ -1,2 +1,2 @@
-cmake -A x64 -B PB\build-x64 -S PB -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
-cmake -A ARM64 -B PB\build-ARM64 -S PB -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake -A x64 -B PB\build-x64 -S PB -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake -A ARM64 -B PB\build-ARM64 -S PB -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
