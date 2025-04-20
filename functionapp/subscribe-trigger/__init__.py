7a79a3e0408742af213d2a1e5e0eda73d57854f8
Cosmin MIHAI <67975221+cosmin42@users.noreply.github.com>
2025-04-20 13:34:29 +0200

    Add app content. (#83)

---
+++ functionapp/subscribe-trigger/__init__.py
@@ -0,0 +1,6 @@
+import logging
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    return func.HttpResponse(f"Success!")
