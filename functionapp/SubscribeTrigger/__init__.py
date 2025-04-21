d5cce857cced826ea08b655e6e44d56f085a36dd
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2025-04-21 12:37:20 +0300

    azure: Insert the emails into the database.

--- functionapp/SubscribeTrigger/__init__.py
+++ functionapp/SubscribeTrigger/__init__.py
@@ -1,6 +1,8 @@
 import logging
-import azure.functions as func
 import re
+import os
+import azure.functions as func
+from azure.data.tables import TableClient
 
 def is_valid_email(email: str) -> bool:
     """
@@ -33,6 +35,21 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             "Invalid email address format.",
             status_code=400
         )
+    
+
+    CONNECTION_STRING = os.getenv("AzureEmailSettings")
+
+    table_client = TableClient.from_connection_string(conn_str=CONNECTION_STRING, table_name="EmailsList")
+
+    email_entity = {
+    "PartitionKey": "Emails",
+    "RowKey": email,
+    "EmailAddress": email,
+    "DateAdded": str(func.datetime.datetime.utcnow())
+    }
+
+    # Insert the entity into the table
+    table_client.create_entity(entity=email_entity)
 
     # If everything is fine, send a success response
     return func.HttpResponse(
