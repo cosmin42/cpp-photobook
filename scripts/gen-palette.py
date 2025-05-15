957eb3a4870ca2614298508594cec10219847fe3
Cosmin Mihai <ionut.cosmin.mihai.pfa@gmail.com>
2025-05-15 22:42:22 +0200

    ui: Update palette script and add more palettes

--- scripts/gen-palette.py
+++ scripts/gen-palette.py
@@ -1,6 +1,39 @@
 import json
 import sys
 
+def parse_color(palette):
+    ui_palette = palette.get("ui", {})
+    fonts_palette = palette.get("fonts", {})
+    return ui_palette, fonts_palette
+
+def print_for_macos_ios(ui_palette, fonts_palette):
+    print("// Generated for SwiftUI")
+    for name, color in ui_palette.items():
+        print(f"    static let {name} = parseColor(hex: \"{color}\")")
+    for name, color in fonts_palette.items():
+        print(f"    static let {name} = parseColor(hex: \"{color}\")")
+
+def print_combined_macos_ios(data):
+    data_macos = data.get("macos", {})
+    data_ios = data.get("ios", {})
+    ui_palette_macos, fonts_palette_macos = parse_color(data_macos)
+    ui_palette_ios, fonts_palette_ios = parse_color(data_ios)
+    print("#if os(macOS)")
+    print_for_macos_ios(ui_palette_macos, fonts_palette_macos)
+    print("#else")
+    print_for_macos_ios(ui_palette_ios, fonts_palette_ios)
+    print("#endif")
+
+
+def print_for_windows(data):
+    data_windows = data.get("windows", {})
+    ui_palette, fonts_palette = parse_color(data_windows)
+    print("<!-- Generated for WinUI3 -->")
+    for name, color in ui_palette.items():
+        print(f"<Color x:Key=\"{name}\">{color}</Color>")
+    for name, color in fonts_palette.items():
+        print(f"<Color x:Key=\"{name}\">{color}</Color>")
+
 def load_palette(file_name, palette_name):
     # Load JSON data from the specified file
     try:
@@ -19,37 +52,8 @@ def load_palette(file_name, palette_name):
         print(f"Error: Palette '{palette_name}' not found.")
         return
     # Print the colors in the chosen palette
-
-    ui_palette = palette.get("ui", {})
-    fonts_palette = palette.get("fonts", {})
-
-    print(f"UI Pallete colors:")
-    for name, color in ui_palette.items():
-        print(f"  {name}: {color}")
-
-    print(f"Fonts Pallete colors:")
-    for name, color in fonts_palette.items():
-        print(f"  {name}: {color}")
-
-    print()
-    print()
-
-    print("<!-- Generated for WinUI3 -->")
-    for name, color in ui_palette.items():
-        print(f"<Color x:Key=\"{name}\">{color}</Color>")
-    for name, color in fonts_palette.items():
-        print(f"<Color x:Key=\"{name}\">{color}</Color>")
-
-    print()
-    print()
-
-    print("// Generated for SwiftUI")
-    print("extension Color {")
-    for name, color in ui_palette.items():
-        print(f"    static let {name} = parseColor(\"hex: {color}\")")
-    for name, color in fonts_palette.items():
-        print(f"    static let {name} = parseColor(\"hex: {color}\")")
-    print("}")
+    print_combined_macos_ios(palette)
+    print_for_windows(palette)
 
 
 # Check command-line arguments
